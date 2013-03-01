57518336a39c5a8c1376dd915a18bd6a775e00ca
Mathias Gelhausen <mathias@xenon>
2013-03-01 13:34:22 +0100

    Add translation in module "Auth", update translation of module "Core", Cleanup core/index/index

--- module/Auth/config/module.config.php
+++ module/Auth/config/module.config.php
@@ -107,6 +107,16 @@ return array(
 //         ),
 //     ),
     
+    'translator' => array(
+        'translation_file_patterns' => array(
+            array(
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ),
+        ),
+    ),
+    
     // Configure the view service manager
     'view_manager' => array(
         'template_path_stack' => array(
