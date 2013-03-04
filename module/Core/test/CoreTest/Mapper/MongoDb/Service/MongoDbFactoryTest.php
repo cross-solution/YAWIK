763c2c8ca9fcf3dd87ce23a0fdcdf58c6d8ef931
Mathias Gelhausen <mathias@xenon>
2013-03-04 16:31:54 +0100

    Add tests for Core module

---
+++ module/Core/test/CoreTest/Mapper/MongoDb/Service/MongoDbFactoryTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Cross Applicant Management - Unit Tests
+ *
+ * @copyright (c) 2013 Cross Solution (http://cross-solution.de)
+ * @license   GPLv3
+ */
+
+namespace CoreTest\Mapper\MongoDb\Service;
+
+class MongoDbFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    
+    public function testFactoryReturnsMongoDbInstance()
+    {
+        $this->assertTrue(true);
+    }
+}
