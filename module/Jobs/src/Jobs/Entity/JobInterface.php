e6bf8280de099b9d5d58b43e52eb79b13883202a
Mathias Gelhausen <gelhausen@cross-solution.de>
2013-10-30 09:29:24 +0100

    [Jobs] Adds property "contactEmail" to job entity
    - Updates job interface

--- module/Jobs/src/Jobs/Entity/JobInterface.php
+++ module/Jobs/src/Jobs/Entity/JobInterface.php
@@ -4,6 +4,7 @@ namespace Jobs\Entity;
 
 use Core\Entity\EntityInterface;
 use Core\Entity\IdentifiableEntityInterface;
+use Core\Entity\CollectionInterface;
 
 interface JobInterface extends EntityInterface, IdentifiableEntityInterface
 {
@@ -11,5 +12,30 @@ interface JobInterface extends EntityInterface, IdentifiableEntityInterface
     public function setApplyId($applyId);
     public function getApplyId();
     
+    public function getLink();
+    public function setLink($link);
+    
+    public function getDatePublishStart();
+    public function setDatePublishStart($datePublishStart);
+    
+    public function getTitle();
+    public function setTitle($title);
+    
+    public function getCompany();
+    public function setCompany($company);
+    
+    public function setContactEmail($email);
+    public function getContactEmail();
+    
+    public function setUserId($userId);
+    public function getUserId();
+    public function injectUser(EntityInterface $user);
+    public function getUser() ;
+    
+    public function setLocation($location);
+    public function getLocation();
+        
+    public function injectApplications(CollectionInterface $applications);
     public function getApplications();
+    
 }
