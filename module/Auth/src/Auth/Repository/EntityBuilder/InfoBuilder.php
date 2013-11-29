fc7cb9b7d0672b615b6c799475b4a095bf5d8a68
Mathias Gelhausen <gelhausen@cross-solution.de>
2013-11-29 12:44:31 +0100

    [Auth] Adds property "image" to InfoEntity
    * If an image id is set in the entity the builder injects a relation entity
      to the contact image.

--- module/Auth/src/Auth/Repository/EntityBuilder/InfoBuilder.php
+++ module/Auth/src/Auth/Repository/EntityBuilder/InfoBuilder.php
@@ -7,13 +7,27 @@ use Core\Repository\EntityBuilder\RelationAwareBuilder;
 use Core\Entity\EntityInterface;
 use Core\Repository\Mapper\MapperAwareInterface;
 use Core\Entity\RelationEntity;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
-class InfoBuilder extends RelationAwareBuilder 
+class InfoBuilder extends RelationAwareBuilder implements MapperAwareInterface
 {
+    
+    protected $mappers;
     
     public function __construct()
     {
         
+    }
+    
+    public function setMapperManager(ServiceLocatorInterface $mapperManager)
+    {
+        $this->mappers = $mapperManager;
+        return $this;
+    }
+    
+    public function getMapperManager()
+    {
+        return $this->mappers;
     }
     
     public function getEntity()
@@ -30,6 +44,18 @@ class InfoBuilder extends RelationAwareBuilder
             $this->setHydrator(new \Core\Repository\Hydrator\EntityHydrator());
         }
         return $this->hydrator;
+    }
+    
+    public function build($data = array())
+    {
+        $entity = parent::build($data);
+        if (!empty($data) && isset($data['imageId'])) {
+            $entity->injectImage(new RelationEntity(
+                array($this->mappers->get('Applications/Files'), 'find'),
+                array($data['imageId'])
+            ));
+        }
+        return $entity;
     }
     
     
