1824e1d9f1e169633a67c88bf4ac684dad0f9a02
Mathias Weitz <weitz@xenon>
2013-12-06 12:18:50 +0100

    JavaScript für das umschalten von Sprachen angepasst

--- module/Core/public/js/core.init.js
+++ module/Core/public/js/core.init.js
@@ -1,26 +1,27 @@
 
 (function($) {
-	
-	initLanguageSwitcher = function()
-	{
-		$("#language-switcher button").click(function(e) {
-			var switchToLang = $(this).attr("id").replace(/^language-switcher-/, "");
-			
-			if ("' . $this->params('lang') . '" != switchToLang) {
-				var langRegex = new RegExp('/' + lang);
-				
-				var newHref=location.protocol
-                       + "//" + location.host + "/"
-                       + location.pathname.replace(langRegex, switchToLang)
-                       + location.search;
-        
-				location.href=newHref;
-			}
+
+    initLanguageSwitcher = function()
+    {
+        $("#language-switcher button").click(function(e) {
+            var switchToLang = '/' + $(this).attr("id").replace(/^language-switcher-/, "");
+
+            if ("' . $this->params('lang') . '" != switchToLang) {
+                var langRegex = new RegExp('/' + lang);
+
+                //console.log(location.pathname, location.pathname.replace(langRegex, switchToLang));
+                var newHref = location.protocol
+                        + "//" + location.host
+                        + location.pathname.replace(langRegex, switchToLang)
+                        + location.search;
+                //console.log(newHref);
+                location.href = newHref;
+            }
         });
-	};
-	
-	$(function() {
-		initLanguageSwitcher();
-	});
-	
+    };
+
+    $(function() {
+        initLanguageSwitcher();
+    });
+
 })(jQuery);
