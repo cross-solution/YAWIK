eef8072932cb256ff31365005a144759465e854d
Mathias Gelhausen <gelhausen@cross-solution.de>
2013-12-13 15:27:06 +0100

    [Jobs] Adds translations

--- module/Jobs/language/_annotated_strings.php
+++ module/Jobs/language/_annotated_strings.php
@@ -1,4 +1,19 @@
 <?php
+//: ./src/Jobs/Form/ListFilterFieldset.php:44
+translate('Show all jobs');
+
+//: ./src/Jobs/Form/ListFilterFieldset.php:45
+translate('Show my jobs');
+
+//: ./src/Jobs/Form/ListFilterFieldset.php:59
+translate('Active');
+
+//: ./src/Jobs/Form/ListFilterFieldset.php:60
+translate('Inactive');
+
+//: ./src/Jobs/Form/ListFilterFieldset.php:71
+translate('Job title');
+
 //: ./config/module.config.php:75
 translate('Jobs');
 
