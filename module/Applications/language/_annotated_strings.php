6a9c6e010e582ab878af36566dbac4ca91018856
Carsten Bleek <bleek@cross-solution.de>
2013-12-16 16:25:12 +0100

    [Applications] adds help texts to the setting forms

--- module/Applications/language/_annotated_strings.php
+++ module/Applications/language/_annotated_strings.php
@@ -23,17 +23,32 @@ translate('E-Mail Notifications');
 //: ./src/Applications/Form/SettingsFieldset.php:29
 translate('receive E-Mail alert');
 
-//: ./src/Applications/Form/SettingsFieldset.php:32
+//: ./src/Applications/Form/SettingsFieldset.php:30
+translate('if checked, you\'ll be informed by mail about new applications.');
+
+//: ./src/Applications/Form/SettingsFieldset.php:34
 translate('Mailtext');
 
 //: ./src/Applications/Form/SettingsFieldset.php:35
+translate('default text of the notification mail about new applications. The following variables can be used:<ul><li>##name## = your name</li><li>##title## = title of the job</li></ul>');
+
+//: ./src/Applications/Form/SettingsFieldset.php:38
 translate('Confirmation mail text');
 
 //: ./src/Applications/Form/SettingsFieldset.php:39
+translate('default text of the acknowledgment of receipt mail to the applicant. The following variables can be used:<br><ul><li>##anrede_formell## = salutation. Includes gender, firstname and lastname.<li>##anrede_informell## = salutation. Includes fistname and lastname.</li><li>##job_title## = title of the jobs</li><li>##name## = name of the applicant.</li><li>##date## = date of recipt of the application.</li></ul>');
+
+//: ./src/Applications/Form/SettingsFieldset.php:43
 translate('Invitation mail text');
 
 //: ./src/Applications/Form/SettingsFieldset.php:44
+translate('default text of the invitation mail to the applicant. You can use all variables of the acknowledgment of receipt mail. ');
+
+//: ./src/Applications/Form/SettingsFieldset.php:49
 translate('Rejection mail text');
+
+//: ./src/Applications/Form/SettingsFieldset.php:50
+translate('default text of the refusal of an application to the applicant. You can use all variables of the acknowledgment of receipt mail.');
 
 //: ./src/Applications/Form/BaseFieldset.php:30
 translate('Summary');
--- module/Applications/src/Applications/Form/SettingsFieldset.php
+++ module/Applications/src/Applications/Form/SettingsFieldset.php
@@ -26,23 +26,28 @@ public function init()
 
         $this->add(array('type' => 'Zend\Form\Element\Checkbox',
         		'name' => 'mailAccess',
-        		'options' => array('label' => /* @translate */ 'receive E-Mail alert')));
+        		'options' => array('label' => /* @translate */ 'receive E-Mail alert',
+        		                   'description' => /* @translate */ 'if checked, you\'ll be informed by mail about new applications.'),
+        		));
         $this->add(array('type' => 'Zend\Form\Element\Textarea',
         		'name' => 'mailAccessText',
-        		'options' => array('label' => /* @translate */ 'Mailtext')));
+        		'options' => array('label' => /* @translate */ 'Mailtext',
+                                            'description' => /* @translate */ 'default text of the notification mail about new applications. The following variables can be used:<ul><li>##name## = your name</li><li>##title## = title of the job</li></ul>')));
         $this->add(array('type' => 'Zend\Form\Element\Textarea',
-            'name' => 'mailConfirmationText',
-            'options' => array('label' => /* @translate */ 'Confirmation mail text')));
+                        'name' => 'mailConfirmationText',
+                         'options' => array('label' => /* @translate */ 'Confirmation mail text',
+                                            'description' => /* @translate */ 'default text of the acknowledgment of receipt mail to the applicant. The following variables can be used:<br><ul><li>##anrede_formell## = salutation. Includes gender, firstname and lastname.<li>##anrede_informell## = salutation. Includes fistname and lastname.</li><li>##job_title## = title of the jobs</li><li>##name## = name of the applicant.</li><li>##date## = date of recipt of the application.</li></ul>' )));
         
         $this->add(array('type' => 'Zend\Form\Element\Textarea',
         		'name' => 'mailInvitationText',
-        		'options' => array('label' => /* @translate */ 'Invitation mail text')));
-        
+        		'options' => array('label' => /* @translate */ 'Invitation mail text',
+        		                    'description'=> /* @translate */ 'default text of the invitation mail to the applicant. You can use all variables of the acknowledgment of receipt mail. '
+        		                    )));
         
         $this->add(array('type' => 'Zend\Form\Element\Textarea',
         		'name' => 'mailRejectionText',
-        		'options' => array('label' => /* @translate */ 'Rejection mail text')));
+        		'options' => array('label' => /* @translate */ 'Rejection mail text',
+                                           'description' => /* @translate */ 'default text of the refusal of an application to the applicant. You can use all variables of the acknowledgment of receipt mail.')));
     }
-    
-    
+        
 }
