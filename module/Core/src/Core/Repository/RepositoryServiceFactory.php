9f7f0cb3e915ea5edb3ee24a8e74ae5d08e66443
Mathias Gelhausen <gelhausen@cross-solution.de>
2014-01-23 13:15:56 +0100

    Revert "[Core] Refactor RepositoryService to allow injecting of dependencies."
    This reverts commit ffddeaa626b697f4abf99a5d96a28f03ba81f462.
    
    (references gh-12)

--- module/Core/src/Core/Repository/RepositoryServiceFactory.php
+++ module/Core/src/Core/Repository/RepositoryServiceFactory.php
@@ -12,18 +12,14 @@ namespace Core\Repository;
 
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
-use Zend\ServiceManager\Config;
 
 class RepositoryServiceFactory implements FactoryInterface
 {
     
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        $configArray = $serviceLocator->get('Config');
-        $configArray = isset($configArray['repositories']) ? $configArray['repositories'] : array();
-        $config      = new Config($configArray);
-        $service     = new RepositoryService();
-        $config->configureServiceManager($service);
+        $dm      = $serviceLocator->get('doctrine.documentmanager.odm_default');
+        $service = new RepositoryService($dm);
         
         return $service;
     }
