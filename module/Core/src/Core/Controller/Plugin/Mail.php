fc5335cbb9b5f5758a461d0b91b3a8a7b3b9a9a2
Mathias Weitz <weitz@xenon>
2014-02-07 16:10:16 +0100

    [Auth] Login Mails for external Logins reworked

--- module/Core/src/Core/Controller/Plugin/Mail.php
+++ module/Core/src/Core/Controller/Plugin/Mail.php
@@ -11,6 +11,7 @@ class mail extends Message implements PluginInterface
 {
      protected $controller;
      protected $param;
+     protected $config;
      
     /**
      * Set the current controller instance
@@ -36,16 +37,39 @@ class mail extends Message implements PluginInterface
     public function __invoke(array $param = array())
     {   
         $this->param = $param;
+        $this->config = array();
         return $this;
     }
     
     public function template($template) {
         $controller =  get_class($this->controller);
+        $services = $this->getController()->getServiceLocator();
+        // get all loaded modules
+        $moduleManager = $services->get('ModuleManager');
+        $loadedModules = $moduleManager->getModules();
         //get_called_class
         $controllerIdentifier = strtolower(substr($controller, 0, strpos($controller, '\\')));
-                
         $viewResolver = $this->getController()->getServiceLocator()->get('ViewResolver');
-        $resource = $viewResolver->resolve($controllerIdentifier . '/mail/' . $template);
+        /*
+        $addModules = array();
+        foreach ($loadedModules as $loadedModule) {
+            $moduleName = strtolower($loadedModule);
+            if ($moduleName != $controllerIdentifier) {
+                $templateFull = $loadedModule . '/view/mail/' . $template;
+                $resource = $viewResolver->resolve($templateFull);
+                $templateFull = $loadedModule . '/mail/' . $template;
+                $resource = $viewResolver->resolve($templateFull);
+            }
+        }
+        */
+                
+        $templateHalf = 'mail/' . $template;
+        $resource = $viewResolver->resolve($templateHalf);
+        
+        if (empty($resource)) {
+            $templateFull = $controllerIdentifier . '/mail/' . $template;
+            $resource = $viewResolver->resolve($templateFull);
+        }
         
         $__vars = $this->param;
         if (array_key_exists('this', $__vars)) {
@@ -64,7 +88,50 @@ class mail extends Message implements PluginInterface
                 ob_end_clean();
                 throw $ex;
             }
+            $__vars = get_defined_vars ();
+            foreach ($this->param as $key => $value) {
+                if (isset($__vars[$key])) {
+                    unset($__vars[$key]);
+                }
+            }
+            unset($__vars['content'],$__vars['controllerIdentifier'],$__vars['controller'],$__vars['resource'],$__vars['template'],$__vars['viewResolver']);
+            $this->config = $__vars;
         }
+    }
+    
+    public function informationComplete() {
+        $log = $this->getController()->getServiceLocator()->get('Log');
+        if (isset($this->config['templateFull'])) {
+            $template = $this->config['templateFull'];
+        }
+        elseif (isset($this->config['templateHalf'])) {
+            $template = $this->config['templateHalf'];
+        }
+        else {
+              throw new \InvalidArgumentException('Not template provided for Mail.');
+        }
+        if (isset($this->config['from'])) {
+            $from = $this->config['from'];
+        }
+        else {
+              throw new \InvalidArgumentException('A from email address must be provided (Variable $from) in Template: ' . $template);
+        }
+        if (isset($this->config['fromName'])) {
+            $fromName = $this->config['fromName'];
+        }
+        else {
+              throw new \InvalidArgumentException('A from name must be provided (Variable $fromName) in Template: ' . $template);
+        }
+        if (isset($this->config['subject'])) {
+            $subject = $this->config['subject'];
+        }
+        else {
+              throw new \InvalidArgumentException('A subject must be provided (Variable $subject) in Template: ' . $template);
+        }
+        
+        $this->setFrom($from, $fromName);
+        $this->setSubject($subject);
+        return $this;
     }
     
     
