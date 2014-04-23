82765a74a965eadd9c938dd79c1f39bed5c70304
Mathias Gelhausen <gelhausen@cross-solution.de>
2014-04-23 10:44:03 +0200

    [Applications] Adds CLI command to (re)calculate rating.
    (refs #5158, gh-33)

--- module/Applications/src/Applications/Controller/ConsoleController.php
+++ module/Applications/src/Applications/Controller/ConsoleController.php
@@ -17,8 +17,7 @@ use Zend\View\Model\JsonModel;
 use Zend\Stdlib\Parameters;
 use Zend\Mvc\MvcEvent;
 use Zend\Console\Request as ConsoleRequest;
-use Zend\ProgressBar\ProgressBar;
-use Zend\ProgressBar\Adapter\Console as ConsoleAdapter;
+use Core\Console\ProgressBar;
 
 /**
  * Main Action Controller for the application.
@@ -30,49 +29,7 @@ class ConsoleController extends AbstractActionController {
     public function generateKeywordsAction() {
         
         $services     = $this->getServiceLocator();
-        $repositories = $services->get('repositories');
-        $appRepo      = $repositories->get('Applications/Application');
-        $filter       = \Zend\Json\Json::decode($this->params('filter', '{}'));
-        $query        = array();
-        $limit        = 0;
-        foreach ($filter as $key => $value) {
-            switch ($key) {
-                case "limit":
-                    $limit = $value;
-                    break;
-                    
-                case "before":
-                    $date = new \DateTime($value);
-                    $q = array('$lt' => $date);
-                    if (isset($query['dateCreated.date'])) {
-                        $query['dateCreated.date']= array_merge(
-                                $query['dateCreated.date'], $q
-                        );
-                    } else {
-                        $query['dateCreated.date'] = $q;
-                    }
-                    break;
-                    
-                case "after":
-                    $date = new \DateTime($value);
-                    $q = array('$gt' => $date);
-                    if (isset($query['dateCreated.date'])) {
-                        $query['dateCreated.date']= array_merge(
-                            $query['dateCreated.date'], $q
-                        );
-                    } else {
-                        $query['dateCreated.date'] = $q;
-                    }
-                    break;
-                                        
-                default:
-                    $query[$key] = $value;
-                    break;
-            }
-        }
-        
-        $applications = $appRepo->findBy($query);
-        $applications->limit($limit);
+        $applications = $this->fetchApplications();
         $count        = $applications->count(true);
 
         if (0 === $count) {
@@ -85,21 +42,7 @@ class ConsoleController extends AbstractActionController {
                 
         echo "Generate keywords for $count applications ...\n";
         
-        $progress     = new ProgressBar(
-            new ConsoleAdapter(array(
-                'elements' => array(
-                    ConsoleAdapter::ELEMENT_TEXT,
-                    ConsoleAdapter::ELEMENT_BAR,
-                    ConsoleAdapter::ELEMENT_PERCENT,
-                    ConsoleAdapter::ELEMENT_ETA
-                ),
-                'textWidth' => 20,
-                'barLeftChar' => '-',
-                'barRightChar' => ' ',
-                'barIndicatorChar' => '>',
-            )),
-            0, count($applications)
-        );
+        $progress     = new ProgressBar($count);
         
         $filter = $services->get('filtermanager')->get('Core/Repository/PropertyToKeywords');
         $i = 0;
@@ -117,10 +60,78 @@ class ConsoleController extends AbstractActionController {
         }
         $progress->update($i, 'Write to database...');
         $repositories->flush();
-        $progress->update($i, 'Done');
         $progress->finish();
         
         return PHP_EOL;   
+    }
+    
+    public function calculateRatingAction()
+    {
+        $applications = $this->fetchApplications();
+        $count = count($applications); $i=0;
+        echo "Calculate rating for " . $count . " applications ...\n";
+        
+        $progress = new ProgressBar($count);
+        
+        foreach ($applications as $application) {
+            $progress->update($i++, 'Application ' . $i . ' / ' . $count);
+            $application->getRating(/* recalculate */ true);
+        }
+        $progress->update($i, 'Write to database...');
+        $this->getServiceLocator()->get('repositories')->flush();
+        $progress->finish();
+        
+        return PHP_EOL;
+    }
+    
+    protected function fetchApplications()
+    {
+        $services     = $this->getServiceLocator();
+        $repositories = $services->get('repositories');
+        $appRepo      = $repositories->get('Applications/Application');
+        $filter       = \Zend\Json\Json::decode($this->params('filter', '{}'));
+        $query        = array();
+        $limit        = 0;
+        foreach ($filter as $key => $value) {
+            switch ($key) {
+                case "limit":
+                    $limit = $value;
+                    break;
+        
+                case "before":
+                    $date = new \DateTime($value);
+                    $q = array('$lt' => $date);
+                    if (isset($query['dateCreated.date'])) {
+                        $query['dateCreated.date']= array_merge(
+                            $query['dateCreated.date'], $q
+                        );
+                    } else {
+                        $query['dateCreated.date'] = $q;
+                    }
+                    break;
+        
+                case "after":
+                    $date = new \DateTime($value);
+                    $q = array('$gt' => $date);
+                    if (isset($query['dateCreated.date'])) {
+                        $query['dateCreated.date']= array_merge(
+                            $query['dateCreated.date'], $q
+                        );
+                    } else {
+                        $query['dateCreated.date'] = $q;
+                    }
+                    break;
+        
+                default:
+                    $query[$key] = $value;
+                    break;
+            }
+        }
+        
+        $applications = $appRepo->findBy($query);
+        $applications->limit($limit);
+        
+        return $applications;
     }
 }
 
