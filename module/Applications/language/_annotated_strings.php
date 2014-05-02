916f8471dd0a1b9b9913b4db89caae1c1ad7a892
Carsten Bleek <bleek@cross-solution.de>
2014-05-02 19:31:37 +0200

    [Applications] rearragement of application details. Moves attachment into an own box. Moves some navigation elements into the "state" box. Adds german translations.

--- module/Applications/language/_annotated_strings.php
+++ module/Applications/language/_annotated_strings.php
@@ -1,26 +1,26 @@
 <?php
-//: ./view/applications/manage/index.ajax.phtml:4
+//: ./view/applications/manage/index.ajax.phtml:5
 translate('Applicant');
 
-//: ./view/applications/manage/index.ajax.phtml:5
+//: ./view/applications/manage/index.ajax.phtml:6
 translate('Career');
 
-//: ./view/applications/manage/index.ajax.phtml:7
+//: ./view/applications/manage/index.ajax.phtml:8
 translate('Attachments');
 
-//: ./view/applications/manage/index.ajax.phtml:9
+//: ./view/applications/manage/index.ajax.phtml:10
 translate('Job');
 
-//: ./view/applications/manage/index.ajax.phtml:11
+//: ./view/applications/manage/index.ajax.phtml:12
 translate('Status');
 
-//: ./view/applications/manage/index.ajax.phtml:13
+//: ./view/applications/manage/index.ajax.phtml:14
 translate('Rating');
 
-//: ./view/applications/manage/index.ajax.phtml:15
+//: ./view/applications/manage/index.ajax.phtml:16
 translate('Modified');
 
-//: ./view/applications/manage/index.ajax.phtml:17
+//: ./view/applications/manage/index.ajax.phtml:18
 translate('Actions');
 
 //: ./src/Applications/Form/PrivacyFieldset.php:49
@@ -41,46 +41,52 @@ translate('Mailtext');
 //: ./src/Applications/Form/SettingsFieldset.php:41
 translate('default text of the notification mail about new applications. The following variables can be used:<ul><li>##name## = your name</li><li>##title## = title of the job</li></ul>');
 
-//: ./src/Applications/Form/SettingsFieldset.php:44
-translate('Confirmation mail text');
-
 //: ./src/Applications/Form/SettingsFieldset.php:45
-translate('default text of the acknowledgment of receipt mail to the applicant. The following variables can be used:<br><ul><li>##anrede_formell## = salutation. Includes gender, firstname and lastname.<li>##anrede_informell## = salutation. Includes fistname and lastname.</li><li>##job_title## = title of the jobs</li><li>##name## = name of the applicant.</li><li>##date## = date of recipt of the application.</li></ul>');
+translate('confirm application immidiatly after submit');
 
-//: ./src/Applications/Form/SettingsFieldset.php:49
-translate('Invitation mail text');
+//: ./src/Applications/Form/SettingsFieldset.php:46
+translate('if checked, an application is immediatly confirmed. If unchecked confirmation is the duty of the recruiter.');
 
 //: ./src/Applications/Form/SettingsFieldset.php:50
-translate('default text of the invitation mail to the applicant. You can use all variables of the acknowledgment of receipt mail. ');
+translate('Confirmation mail text');
+
+//: ./src/Applications/Form/SettingsFieldset.php:51
+translate('default text of the acknowledgment of receipt mail to the applicant. The following variables can be used:<br><ul><li>##anrede_formell## = salutation. Includes gender, firstname and lastname.<li>##anrede_informell## = salutation. Includes fistname and lastname.</li><li>##job_title## = title of the jobs</li><li>##name## = name of the applicant.</li><li>##date## = date of recipt of the application.</li></ul>');
 
 //: ./src/Applications/Form/SettingsFieldset.php:55
-translate('Rejection mail text');
+translate('Invitation mail text');
 
 //: ./src/Applications/Form/SettingsFieldset.php:56
-translate('default text of the refusal of an application to the applicant. You can use all variables of the acknowledgment of receipt mail.');
-
-//: ./src/Applications/Form/SettingsFieldset.php:60
-translate('get blind carbon copy of all own mails');
+translate('default text of the invitation mail to the applicant. You can use all variables of the acknowledgment of receipt mail. ');
 
 //: ./src/Applications/Form/SettingsFieldset.php:61
+translate('Rejection mail text');
+
+//: ./src/Applications/Form/SettingsFieldset.php:62
+translate('default text of the refusal of an application to the applicant. You can use all variables of the acknowledgment of receipt mail.');
+
+//: ./src/Applications/Form/SettingsFieldset.php:66
+translate('get blind carbon copy of all own mails');
+
+//: ./src/Applications/Form/SettingsFieldset.php:67
 translate('if checked, you\'ll get a copy of all mails you send.');
 
 //: ./src/Applications/Form/CommentForm.php:41
 translate('Comment message');
 
-//: ./src/Applications/Form/CreateApplication.php:105
+//: ./src/Applications/Form/CreateApplication.php:112
 translate('send application');
 
 //: ./src/Applications/Form/BaseFieldset.php:30
 translate('Summary');
 
-//: ./src/Applications/Controller/IndexController.php:107
+//: ./src/Applications/Controller/IndexController.php:108
 translate('There were errors in the form.');
 
-//: ./src/Applications/Controller/IndexController.php:160
+//: ./src/Applications/Controller/IndexController.php:163
 translate('Application confirmation');
 
-//: ./src/Applications/Controller/IndexController.php:175
+//: ./src/Applications/Controller/IndexController.php:195
 translate('your application was sent successfully');
 
 //: ./src/Applications/Mail/Forward.php:40
@@ -92,9 +98,12 @@ translate('New application for your vacancy "%s"');
 //: ./src/Applications/Mail/NewApplication.php:62
 translate('Hello ##name##,\n\nThere is a new application for your vacancy:\n\"##title##\"\n\n');
 
-//: ./config/module.config.php:45
+//: ./src/Applications/Mail/ApplicationCarbonCopy.php:24
+translate('Application to "%s" dated %s');
+
+//: ./config/module.config.php:46
 translate('E-Mail Templates');
 
-//: ./config/module.config.php:95
+//: ./config/module.config.php:99
 translate('Overview');
 
