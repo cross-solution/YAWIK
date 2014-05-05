aae09b21c61819426e2a9d18729aa0ef78afb864
Mathias Gelhausen <gelhausen@cross-solution.de>
2014-05-05 16:29:39 +0200

    [Applications] Adds job ad autocompletion input field to applications list filter.
    (references #5196, gh-36)

--- module/Applications/public/js/applications.index.js
+++ module/Applications/public/js/applications.index.js
@@ -1,8 +1,69 @@
 
 ;(function($) {
 	
+	function initializeJobsSelectField()
+	{
+		var selectedValue = null;
+		
+		var jobs = new Bloodhound({
+			name: 'jobs',
+			remote: basePath + '/' + lang + '/jobs/typeahead?q=%QUERY',
+			datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+			queryTokenizer: Bloodhound.tokenizers.whitespace
+		});
+		
+		jobs.initialize();
+		
+		var filterDisplayText = function(d) {
+			return '[' + d.applyId + '] ' + d.title; 
+		};
+		
+		$('#search-applications-form #job-filter').typeahead(
+			{
+				highlight: true,
+				minLength: 4,
+			},
+			{
+				name: 'jobs',
+				displayKey: filterDisplayText,
+				source: jobs.ttAdapter(),
+				templates: {
+					suggestion: function(d) {
+						return '<p>' + d.title + '<br><small>' + d.applyId + ' | ' + d.id + '</small></p>';
+					}
+
+				}
+			}
+		).on('typeahead:selected', function(e, d, n) {
+			selectedValue = filterDisplayText(d);
+			$('#job-filter-value').val(d.id);
+		}).on('blur', function(e) {
+			if (selectedValue != $(this).val()) { 
+				$(this).val('');
+				selectedValue = '';
+				$('#job-filter-value').val('');
+			}
+		}).on('focus', function (e) {
+		    $(this).on('mouseup', function(e) {
+		    	e.preventDefault();
+		    	$(this).off('mouseup');
+		    }).select();
+		});
+		if ('' != $('#search-applications-form #job-filter').typeahead('val')) {
+			selectedValue = $('#search-applications-form #job-filter').typeahead('val');
+		}
+		
+		
+		
+		$('#search-applications-form').submit(function() {
+			$(this).find('#job-filter').prop("disabled", true);
+		});
+		
+	}
+	
 	$(function() {
 		$('#search-applications span').popover();
+		initializeJobsSelectField();
 	});
 	
 })(jQuery);
