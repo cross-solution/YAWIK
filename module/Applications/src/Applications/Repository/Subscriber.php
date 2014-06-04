60a7cd939a70de075a57656b33a16c02663f491b
Mathias Weitz <weitz@cross-solution.de>
2014-06-04 18:26:23 +0200

    [application] take the subscriber-uri as it is, with no transformation or interpretation of any kind

--- module/Applications/src/Applications/Repository/Subscriber.php
+++ module/Applications/src/Applications/Repository/Subscriber.php
@@ -62,35 +62,57 @@ class Subscriber extends AbstractProviderRepository
     {
         // @TODO, uri must be a real place, with no regular expressions
         $services = $this->serviceLocator;
-        $config = $services->get('config');
-        $portal = 0;
+//        $config = $services->get('config');
+//        $portal = 0;
+//        $name = '';
+//        if (preg_match('/^.*?(\d+)$/', $uri, $matches)) {
+//            $portal = (int) $matches[1];
+//        }
+//        if ($portal == 0) {
+//            //$services->get('Log/Core/Cam')->info('Applications/getSubscriberName: ' . $url . ', returned: ' . $status);
+//            return '';
+//        }
+//        if (isset($config['Applications']) && isset($config['Applications']['getSubscriberName'])) {
+//            try {
+//                $url = $config['Applications']['getSubscriberName'];
+//                $client = new \Zend\Http\Client($url . $portal);
+//                $client->setMethod('GET');
+//                $response = $client->send();
+//                $status = $response->getStatusCode();
+//                if ($status == 200) {
+//                    $result = $response->getBody();
+//                    $result = json_decode($result);
+//                    if (property_exists($result, $portal)) {
+//                        $name = $result->{$portal};
+//                    }
+//                } else {
+//                    $services->get('Log/Core/Cam')->err('Applications/getSubscriberName: ' . $url . ', returned: ' . $status);
+//                }
+//            } catch (\Exception $e) {
+//                
+//            }
+//        }
+
         $name = '';
-        if (preg_match('/^.*?(\d+)$/', $uri, $matches)) {
-            $portal = (int) $matches[1];
-        }
-        if ($portal == 0) {
-            //$services->get('Log/Core/Cam')->info('Applications/getSubscriberName: ' . $url . ', returned: ' . $status);
-            return '';
-        }
-        if (isset($config['Applications']) && isset($config['Applications']['getSubscriberName'])) {
-            try {
-                $url = $config['Applications']['getSubscriberName'];
-                $client = new \Zend\Http\Client($url . $portal);
-                $client->setMethod('GET');
-                $response = $client->send();
-                $status = $response->getStatusCode();
-                if ($status == 200) {
-                    $result = $response->getBody();
-                    $result = json_decode($result);
-                    if (property_exists($result, $portal)) {
-                        $name = $result->{$portal};
-                    }
-                } else {
-                    $services->get('Log/Core/Cam')->err('Applications/getSubscriberName: ' . $url . ', returned: ' . $status);
+        try {
+            $client = new \Zend\Http\Client($uri);
+            $client->setMethod('GET');
+            $response = $client->send();
+            $status = $response->getStatusCode();
+            if ($status == 200) {
+                $result = $response->getBody();
+                $result = (array) json_decode($result);
+                if (0 < count($result)) {
+                    $name = array_pop($result);
                 }
-            } catch (\Exception $e) {
-                
+                //if (property_exists($result, 'portal')) {
+                //    $name = $result->portal;
+                //}
+            } else {
+                $services->get('Log/Core/Cam')->err('Applications/getSubscriberName: ' . $uri . ', returned: ' . $status);
             }
+        } catch (\Exception $e) {
+            
         }
         return $name;
     }
