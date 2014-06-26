6ebeebcbdafb276f6dec1b4b125d76f9aa6604c7
Mathias Weitz <weitz@cross-solution.de>
2014-06-26 10:04:00 +0200

    add company modul (#5327)

---
+++ module/Companies/config/router.config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @copyright (c) 2013-2104 Cross Solution (http://cross-solution.de)
+ * @license   AGPLv3
+ */
+return array(
+    'router' => array(
+        'routes' => array(
+            'lang' => array(
+                'child_routes' => array(
+                )
+            )
+        )
+    )
+);
