37f83b557ca32f41dafd89815f0190ef7e32fc04
Rafal Ksiazek <harpcio@gmail.com>
2015-01-07 11:47:47 +0100

    Y83 - tests

--- module/Jobs/Module.php
+++ module/Jobs/Module.php
@@ -9,20 +9,15 @@
 
 namespace Jobs;
 
-
 use Zend\Console\Adapter\AdapterInterface as Console;
 use Zend\ModuleManager\Feature\ConsoleUsageProviderInterface;
 use Core\ModuleManager\ModuleConfigLoader;
-use Zend\Mvc\MvcEvent;
-use Jobs\Listener\UpdateJobsPermissionsListener;
 
 /**
  * Bootstrap class of the Core module
- * 
  */
 class Module implements ConsoleUsageProviderInterface
 {
-    
 
     public function getConsoleUsage(Console $console)
     {
@@ -30,16 +25,16 @@ class Module implements ConsoleUsageProviderInterface
             'Manipulation of jobs database',
             'jobs generatekeywords [--filter=]' => '(Re-)Generates keywords for all jobs.',
             array('--filter=JSON', "available keys:\n"
-                                  ."- 'before:ISODate' -> only jobs before the given date\n"
-                                  ."- 'after':ISODate' -> only jobs after the given date\n"
-                                  ."- 'title':String -> exakt title to match or if starting with '/' -> MongoRegex\n"
-                                  ."- 'limit':INT -> Limit result."),
+                . "- 'before:ISODate' -> only jobs before the given date\n"
+                . "- 'after':ISODate' -> only jobs after the given date\n"
+                . "- 'title':String -> exakt title to match or if starting with '/' -> MongoRegex\n"
+                . "- 'limit':INT -> Limit result."),
         );
     }
-    
+
     /**
      * Loads module specific configuration.
-     * 
+     *
      * @return array
      */
     public function getConfig()
@@ -49,21 +44,18 @@ class Module implements ConsoleUsageProviderInterface
 
     /**
      * Loads module specific autoloader configuration.
-     * 
+     *
      * @return array
      */
     public function getAutoloaderConfig()
     {
-        
         return array(
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                    __NAMESPACE__ . 'Test' => __DIR__ . '/test/' . __NAMESPACE__ . 'Test',
                 ),
             ),
         );
     }
-    
-   
-    
 }
---
+++ module/Organizations/test/OrganizationsTest/Entity/Provider/OrganizationEntityProvider.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @copyright (c) 2013-2014 Cross Solution (http://cross-solution.de)
+ * @license   MIT
+ */
+
+namespace OrganizationsTest\Entity\Provider;
+
+use Organizations\Entity\Organization;
+
+class OrganizationEntityProvider
+{
+    /**
+     * @param array $params
+     *
+     * @return Organization
+     */
+    public static function createEntityWithRandomData(array $params = array())
+    {
+        $params = static::createNewRelations($params);
+
+        $withId = true;
+        $entityId = bin2hex(substr(uniqid(), 1));
+        // define here another variables
+
+        extract($params);
+
+        $organizationEntity = new Organization();
+        // here set another variables
+
+        if (!empty($organizationName)) {
+            $organizationEntity->setOrganizationName($organizationName);
+        }
+
+        if ($withId) {
+            $organizationEntity->setId($entityId);
+        }
+
+        return $organizationEntity;
+    }
+
+    private static function createNewRelations(array $params = array())
+    {
+        extract($params);
+
+        if (!empty($createOrganizationName)) {
+            $organizationName = OrganizationNameEntityProvider::createEntityWithRandomData((array)$createOrganizationName);
+        }
+
+        return array_merge($params, compact('organizationName'));
+    }
+}
