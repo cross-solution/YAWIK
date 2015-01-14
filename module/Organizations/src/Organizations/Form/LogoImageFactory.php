1218fe42941f7ce450b19fda0329893a7edf8a7c
Rafal Ksiazek <harpcio@gmail.com>
2015-01-14 12:22:40 +0100

    Upgrading organization form - css fix

--- module/Organizations/src/Organizations/Form/LogoImageFactory.php
+++ module/Organizations/src/Organizations/Form/LogoImageFactory.php
@@ -9,10 +9,28 @@
 
 namespace Organizations\Form;
 
-use Auth\Form\UserImageFactory;
+use Core\Form\FileUploadFactory;
+use Core\Form\Form;
 
-class LogoImageFactory extends UserImageFactory
+class LogoImageFactory extends FileUploadFactory
 {
+    protected $fileName = 'image';
     protected $fileEntityClass = '\Organizations\Entity\OrganizationImage';
     protected $configKey = 'organization_logo_image';
+
+    protected function configureForm(Form $form)
+    {
+        $size = isset($this->config['max_size']) ? $this->config['max_size'] : 100000;
+        $type = isset($this->config['mimetype']) ? $this->config['mimetype'] : 'image';
+
+        $form->get($this->fileName)->setViewHelper('FormImageUpload')
+            ->setMaxSize($size)
+            ->setAllowedTypes($type)
+            ->setForm($form);
+
+        $form->setIsDescriptionsEnabled(true);
+        $form->setDescription(
+            /*@translate*/ 'Choose a Logo. This logo will be shown in the job opening and the application form.'
+        );
+    }
 } 
