ee41a88c06a67f3f3c207b71c53e7afceea55c8f
root <root@cross-solution.de>
2015-01-18 12:35:44 +0100

    [CI] adds a build properties file for travis

--- install.sh
+++ install.sh
@@ -1,8 +1,32 @@
-#!/bin/sh
-
+#!/bin/bash
 
 PHING=phing.phar
 COMPOSER=composer.phar
+PROPERTIES=build.properties
+
+function usage {
+  echo "";
+  echo "-b|--build-properties       Location of the build.properties file";
+  echo "-h|--help                   this usage";
+  echo "";
+  exit;  
+}
+
+
+while [ "$1" != "" ]; do
+  case $1 in
+   -b | --build-properties )
+   	shift
+   	PROPERTIES=$1
+        ;;
+   -h | --help )
+   	usage
+        exit
+        ;;
+   esac
+   shift
+done
+
 
 #
 # assume, the script is running within a development environment, if ".git"
@@ -10,9 +34,16 @@ COMPOSER=composer.phar
 #
 # Note: this leads to a problem, if you install YAWIK on eg. openshift. 
 #
-if [ -d .git ] 
+if [ -d .git ] || [ "$1"=="devenv" ]; 
 then
   DEVENV=1
+fi;
+
+
+if [ ! -f $PROPERTIES ];
+then
+  echo "no properties file found at $PROPERTIES. exit now."
+  exit 1;
 fi;
 
 #
@@ -29,6 +60,9 @@ fi;
 # Download Composer
 #
 
+
+echo "Installing úsing: $PROPERTIES"
+
 if [ $DEVENV ]
 then
 
@@ -38,8 +72,11 @@ then
 		curl -sS https://getcomposer.org/installer | php
 	fi;
 
-	./$PHING
+	./$PHING -Dbuild.properties $PROPERTIES
 	
 else
-	./$PHING generate-autoload-config
+	./$PHING -Dbuild.properties $PROPERTIES generate-autoload-config
 fi;
+
+
+          
