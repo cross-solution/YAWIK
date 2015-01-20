3410ac7d9a5cb79adf885d8f1c5ce9ad21086ca8
Rafal Ksiazek <harpcio@gmail.com>
2015-01-21 00:34:31 +0100

    Small fixes

---
+++ module/Auth/public/js/form.change-password.js
@@ -0,0 +1,8 @@
+var $form = $('#user-password-form');
+$form.on('yk.forms.done', function (event, object) {
+    event.stopPropagation();
+    new PNotify({
+        text: object.data.text,
+        type: object.data.status
+    });
+});
