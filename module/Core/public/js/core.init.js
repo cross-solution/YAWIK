901843103048a1d88c0ccf6450f3b1f32a12e3f7
Rafal Ksiazek <harpcio@gmail.com>
2015-01-23 19:24:28 +0100

    Pnotify small fixes

--- module/Core/public/js/core.init.js
+++ module/Core/public/js/core.init.js
@@ -1,26 +1,28 @@
+(function ($) {
 
-(function($) {
-
-    initLanguageSwitcher = function()
-    {
-        $("#language-switcher button").click(function(e) {
+    initLanguageSwitcher = function () {
+        $("#language-switcher button").click(function (e) {
             var switchToLang = '/' + $(this).attr("id").replace(/^language-switcher-/, "");
 
             if (lang != switchToLang) {
                 var langRegex = new RegExp('/' + lang + '($|\/)');
 
                 var newHref = location.protocol
-                        + "//" + location.host
-                        + location.pathname.replace(langRegex, switchToLang + '$1')
-                        + location.search;
+                    + "//" + location.host
+                    + location.pathname.replace(langRegex, switchToLang + '$1')
+                    + location.search;
                 //console.log(newHref);
                 location.href = newHref;
             }
         });
     };
+    initPnotify = function () {
+        PNotify.prototype.options.styling = "fontawesome";
+    };
 
-    $(function() {
+    $(function () {
         initLanguageSwitcher();
+        initPnotify();
     });
 
 })(jQuery);
