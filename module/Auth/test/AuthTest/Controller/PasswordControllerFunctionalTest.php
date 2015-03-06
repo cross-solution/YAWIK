7e98e101190f4c89332d51370ee5883cfa2d0390
Rafal Ksiazek <harpcio@gmail.com>
2015-03-06 01:05:59 +0100

    Functional tests

---
+++ module/Auth/test/AuthTest/Controller/PasswordControllerFunctionalTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AuthTest\Controller;
+
+use CoreTest\Controller\AbstractFunctionalControllerTestCase;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+use Zend\Http\PhpEnvironment\Request;
+use Zend\Http\PhpEnvironment\Response;
+
+class PasswordControllerFunctionalTest extends AbstractFunctionalControllerTestCase
+{
+    const URL_MY_PASSWORD = '/en/my/password';
+
+    /**
+     * @var MockObject
+     */
+    private $repositoriesMock;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->repositoriesMock = $this->getMockBuilder('Core\Repository\RepositoryService')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->setMockToServiceLocator('repositories', $this->repositoriesMock);
+    }
+
+    public function testAction()
+    {
+        $this->dispatch(self::URL_MY_PASSWORD, Request::METHOD_GET);
+
+        $result = $this->getResponse()->getContent();
+
+        $this->assertNotRedirect();
+        $this->assertResponseStatusCode(Response::STATUS_CODE_403);
+        $this->assertContains('Please authenticate yourself to proceed', $result);
+    }
+}
---
+++ module/Core/test/CoreTest/Controller/AbstractFunctionalControllerTestCase.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace CoreTest\Controller;
+
+use Auth\Entity\AnonymousUser;
+use Auth\Entity\User;
+use AuthTest\Entity\Provider\UserEntityProvider;
+use Test\Bootstrap;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+use Zend\ServiceManager\ServiceManager;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+abstract class AbstractFunctionalControllerTestCase extends AbstractHttpControllerTestCase
+{
+    /**
+     * @var ServiceManager
+     */
+    protected $serviceLocator;
+
+    /**
+     * @var MockObject
+     */
+    private $documentManagerMock;
+
+    public function setUp()
+    {
+        $this->serviceLocator = null;
+        $this->setApplicationConfig(
+            Bootstrap::getConfig()
+        );
+        parent::setUp();
+        $this->prepareAuthenticateMock();
+        $this->prepareDocumentManagerMock();
+    }
+
+    /**
+     * Set service to service locator
+     *
+     * @param string $name
+     * @param object $object
+     *
+     * @return ServiceManager
+     */
+    protected function setMockToServiceLocator($name, $object)
+    {
+        if (!$this->serviceLocator) {
+            $this->serviceLocator = $this->getApplicationServiceLocator();
+            $this->serviceLocator->setAllowOverride(true);
+        }
+        $this->serviceLocator->setService($name, $object);
+        return $this->serviceLocator;
+    }
+
+    /**
+     * Creates and authenticates a user.
+     *
+     * @param array $params
+     *
+     * @return User
+     */
+    protected function authenticateUser(array $params = [])
+    {
+        $userEntity = UserEntityProvider::createEntityWithRandomData($params);
+        $this->prepareAuthenticateMock(true, $userEntity);
+        return $userEntity;
+    }
+
+    /**
+     * @param bool $hasIdentity
+     * @param User $userEntity
+     */
+    protected function prepareAuthenticateMock($hasIdentity = false, User $userEntity = null)
+    {
+        if (null === $userEntity) {
+            $userEntity = new AnonymousUser();
+        }
+
+        $authMock = $this->getMockBuilder('Auth\AuthenticationService')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $authMock->expects($this->any())
+            ->method('hasIdentity')
+            ->will($this->returnValue($hasIdentity));
+
+        $authMock->expects($this->any())
+            ->method('getIdentity')
+            ->will($this->returnValue($userEntity));
+
+        $authMock->expects($this->any())
+            ->method('getUser')
+            ->will($this->returnValue($userEntity));
+
+        $this->setMockToServiceLocator('AuthenticationService', $authMock);
+    }
+
+    protected function prepareDocumentManagerMock()
+    {
+        $this->documentManagerMock = $this->getMockBuilder('Doctrine\ODM\MongoDB\DocumentManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->setMockToServiceLocator('Core\DocumentManager', $this->documentManagerMock);
+    }
+}
