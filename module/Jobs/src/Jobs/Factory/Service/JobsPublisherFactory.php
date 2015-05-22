eb1d5ca48f47cf9bca3d4a95dee4fc91d46481d1
Mathias Weitz <weitz@cross-solution.de>
2015-05-22 17:38:55 +0200

    [Jobs] post channels

--- module/Jobs/src/Jobs/Factory/Service/JobsPublisherFactory.php
+++ module/Jobs/src/Jobs/Factory/Service/JobsPublisherFactory.php
@@ -29,25 +29,48 @@ class JobsPublisherFactory extends RestClientFactory
 
     protected function getUri() {
         $config = $this->getConfig();
-        if (!array_key_exists('uri', $config)) {
-            throw new \RuntimeException('uri for Rest-Server YAWIK is missing', 500);
+        if (!array_key_exists('host', $config)) {
+            throw new \RuntimeException('host is missing', 500);
         }
-        return $config['uri'];
+        if (!array_key_exists('location', $config)) {
+            throw new \RuntimeException('location is missing', 500);
+        }
+        return $config['host'] . '/' . $config['location'];
     }
 
     protected function getConfig() {
-        if (!isset($this->config)) {
-            $config = $this->serviceLocator->get('Config');
-            if (!array_key_exists('multiposting', $config)) {
-                throw new \RuntimeException('configuration for multiposting is missing', 500);
+        $jobsOptions = $this->serviceLocator->get('Jobs/Options');
+
+        if (!isset($this->multipostingTarget) && isset($jobsOptions->multipostingTargetUri)) {
+            // The Uri has this Form
+            // http://user:pass@host/location?query
+            if (preg_match('/^https*:\/\/([^:]*):([^@]*)@([^\/]*)\/([^\?]*)\??(.*)$/i', $jobsOptions->multipostingTargetUri, $elements)) {
+                $user = $elements[1];
+                $password = $elements[2];
+                $host = $elements[3];
+                $location = $elements[4];
+                $query = $elements[5];
+
+
+                /*
+                if (!array_key_exists('multiposting', $config)) {
+                    throw new \RuntimeException('configuration for multiposting is missing', 500);
+                }
+                if (!array_key_exists('target', $config['multiposting'])) {
+                    throw new \RuntimeException('target for multiposting is missing', 500);
+                }
+                if (!array_key_exists('restServer', $config['multiposting']['target'])) {
+                    throw new \RuntimeException('configuration restServer for multiposting.target is missing', 500);
+                }
+                */
+                $this->config = array(
+                    'PHP_AUTH_USER' => $user,
+                    'PHP_AUTH_PW' => $password,
+                    'host' => $host,
+                    'location'=> $location,
+                    'query' => $query
+                );
             }
-            if (!array_key_exists('target', $config['multiposting'])) {
-                throw new \RuntimeException('target for multiposting is missing', 500);
-            }
-            if (!array_key_exists('restServer', $config['multiposting']['target'])) {
-                throw new \RuntimeException('configuration restServer for multiposting.target is missing', 500);
-            }
-            $this->config = $config['multiposting']['target']['restServer'];
         }
         return $this->config;
     }
