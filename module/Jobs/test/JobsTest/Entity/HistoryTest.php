ce0745d8d6a439ccd4ffcbdf777fa9ab8dc917e8
Carsten Bleek <bleek@cross-solution.de>
2015-06-23 09:36:20 +0200

    [Jobs] adds Unit tests for History and Status Entity

---
+++ module/Jobs/test/JobsTest/Entity/HistoryTest.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @license    MIT
+ * @copyright  2013 - 2015 Cross Solution <http://cross-solution.de>
+ */
+
+/** */
+namespace JobsTest\Entity;
+
+use Jobs\Entity\History;
+use Jobs\Entity\Status;
+
+/**
+ * Tests for Status
+ *
+ * @covers \Jobs\Entity\History
+ * @coversDefaultClass \Jobs\Entity\History
+ *
+ * @author Carsten Bleek <bleek@cross-solution.de>
+ * @group  Jobs
+ * @group  Jobs.Entity
+ */
+class HistoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * The "Class under Test"
+     *
+     * @var History
+     */
+    private $target;
+
+    public function setup()
+    {
+        $this->target = new History("CREATED");
+    }
+
+    /**
+     * @testdox Extends \Core\Entity\AbstractEntity and implements \Jobs\Entity\HistoryInterface
+     * @coversNothing
+     */
+    public function testExtendsAbstractEntityAndImplementsStatusInterface()
+    {
+        $this->assertInstanceOf('\Core\Entity\AbstractEntity', $this->target);
+        $this->assertInstanceOf('\Jobs\Entity\HistoryInterface', $this->target);
+    }
+
+    public function provideCreatingInstancesTestData()
+    {
+        return array(
+            array("CREATED",              'message1'  , new Status("CREATED"),              "message1"),
+            array("WAITING_FOR_APPROVAL", 'message2'  , new Status("WAITING_FOR_APPROVAL"), "2"),
+            array("REJECTED",             'message3'  , new Status("REJECTED"),             "3"),
+            array("PUBLISH",              'message4'  , new Status("PUBLISH"),              "4"),
+            array("ACTIVE",               'message5'  , new Status("ACTIVE"),               "5"),
+            array("INACTIVE",             'message6'  , new Status("INACTIVE"),             "6"),
+            array("EXPIRED",              'message7'  , new Status("EXPIRED"),              "7"),
+        );
+    }
+
+    /**
+     * @testdox      Can be constructed in all possible states
+     * @dataProvider provideCreatingInstancesTestData
+     * @covers ::__construct
+     *
+     * @param string $status           the status
+     * @param string $message          the message
+     * @param string $expectedStatus   the expected status
+     * @param string $expectedMessage  the expected message
+     */
+    public function testCreatingInstances($status, $message, $expectedStatus, $expectedMessage)
+    {
+        $target = null === $status ? new History() : new History($status);
+
+        $this->assertAttributeEquals($expectedStatus, 'status', $target);
+//        $this->assertAttributeEquals($expectedMessage, 'message', $target);
+    }
+
+
+    /**
+     * @testdox Status() throws an exception if invalid status is passed.
+     * @expectedException DomainException
+     * @expectedExceptionMessage Unknown status:
+     */
+    public function testStatusThrowsExceptionIfInvalidStatusPassed()
+    {
+        $target = new History('highly invalid status name');
+    }
+    
+    
+    /**
+    * @covers Jobs\Entity\History::setDate
+    * @covers Jobs\Entity\History::getDate
+    */
+    public function testSetGetDate()
+    {
+        $date=new \DateTime();
+        $this->target->setDate($date);
+
+        $this->assertEquals($date, $this->target->getDate());        
+    }
+    
+    /**
+    * @covers Jobs\Entity\History::setMessage
+    * @covers Jobs\Entity\History::getMessage
+    */
+    public function testSetGetMessage()
+    {
+        $message="my message";
+        $this->target->setMessage($message);
+
+        $this->assertEquals($message, $this->target->getMessage());        
+    }
+    
+    /**
+    * @covers Jobs\Entity\History::setStatus
+    * @covers Jobs\Entity\History::getStatus
+    */
+    public function testSetGetStatus()
+    {
+        $status=new Status("CREATED");
+        $this->target->setStatus($status);
+        $this->assertEquals($status, $this->target->getStatus());        
+    }
+}
