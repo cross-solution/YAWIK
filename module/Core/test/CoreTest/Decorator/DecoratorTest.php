943d816dcdf30447ca8c522758b17f7f8f7d8506
Mathias Gelhausen <gelhausen@cross-solution.de>
2015-07-07 14:09:10 +0200

    [Core] Adds Decorator boilerplates.

---
+++ module/Core/test/CoreTest/Decorator/DecoratorTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @license MIT
+ * @copyright  2013 - 2015 Cross Solution <http://cross-solution.de>
+ */
+  
+/** */
+namespace CoreTest\Decorator;
+
+use Core\Decorator\Decorator;
+
+/**
+ * Tests for \Core\Decorator\Decorator
+ * 
+ * @covers \Core\Decorator\Decorator
+ * @author Mathias Gelhausen <gelhausen@cross-solution.de>
+ * @group Core
+ * @group Core.Decorator
+ */
+class DecoratorTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testConstruction()
+    {
+        $object = new \stdClass();
+        $target = new Decorator($object);
+
+        $this->assertAttributeEquals($object, 'object', $target);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Wrapped entity must be of type \stdClass
+     */
+    public function testConstructionThrowsErrorIfObjectTypeIsNotValid()
+    {
+        new Decorator(array());
+    }
+}
