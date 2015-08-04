7e8e4ce7b301c59c9c62eee4a39447cb27a12760
Mathias Gelhausen <gelhausen@cross-solution.de>
2015-08-04 17:09:14 +0200

    [Core] Adds "systemMessageEmail option to core module options.
    * Adds Exception MissingOptionException under the \Core\Options namespace.

--- module/Core/src/Core/Options/ModuleOptions.php
+++ module/Core/src/Core/Options/ModuleOptions.php
@@ -1,15 +1,16 @@
 <?php
 /**
  * YAWIK
- * 
+ *
  * @filesource
  * @copyright (c) 2013-2015 Cross Solution (http://cross-solution.de)
- * @license   MIT
- * @author    weitz@cross-solution.de
+ * @license       MIT
+ * @author        weitz@cross-solution.de
  */
 
 namespace Core\Options;
 
+use Core\Options\Exception\MissingOptionException;
 use Zend\Stdlib\AbstractOptions;
 
 /**
@@ -17,32 +18,35 @@ use Zend\Stdlib\AbstractOptions;
  *
  * Default options of the Core Module
  *
- * @package Core\Options
+ * @author Carsten Bleek <bleek@cross-solution.de>
+ * @author Mathias Weitz <weitz@cross-solution.de>
+ * @author Mathias Gelhausen <gelhausen@cross-solution.de>
  */
-class ModuleOptions extends AbstractOptions {
+class ModuleOptions extends AbstractOptions
+{
 
     /**
      * The sitename is used in Mails. Typically it's the name of your website
      *
      * @var string
      */
-    protected $siteName="YAWIK";
+    protected $siteName = "YAWIK";
 
     /**
      * Contact Data, which can be used in Mail signatures or the imprint page.
      *
      * @var array $operator
      */
-    protected $operator=array(
-        'companyShortName'=>'Your Company Name',
-        'companyFullName' => 'Your Company Name Ltd. & Co KG',
-        'companyTax' => 'Your VAT Number',
-        'postalCode' => 'xxxx',
-        'city' => '',
-        'street'=> '',
-        'name' => '',
-        'email' => '',
-        'fax' => ''
+    protected $operator = array(
+        'companyShortName' => 'Your Company Name',
+        'companyFullName'  => 'Your Company Name Ltd. & Co KG',
+        'companyTax'       => 'Your VAT Number',
+        'postalCode'       => 'xxxx',
+        'city'             => '',
+        'street'           => '',
+        'name'             => '',
+        'email'            => '',
+        'fax'              => ''
     );
 
     /**
@@ -77,45 +81,45 @@ class ModuleOptions extends AbstractOptions {
      *
      * @var string
      */
-    protected $defaultCurrencyCode ="USD";
+    protected $defaultCurrencyCode = "USD";
 
     /**
      * default tax rate used, if no tax rate is set
      *
      * @var string
      */
-    protected $defaultTaxRate ="19";
-
+    protected $defaultTaxRate = "19";
 
     /**
-     * @param $siteName
-     * @return $this
+     * Email address to send system messages to.
+     *
+     * @var string
      */
-    public function setSiteName($siteName) {
-        $this->siteName = $siteName;
-        return $this;
-    }
+    protected $systemMessageEmail;
 
     /**
      * @return string
+     * @throws MissingOptionException
+     * @since 0.20 throws MissingOptionException instead of InvalidArgumentException
      */
-    public function getSiteName() {
+    public function getSiteName()
+    {
         if (empty($this->siteName)) {
-                throw new \InvalidArgumentException(
-                    'the argument sitename has to be defined'
-                );
+            throw new MissingOptionException('siteName', $this);
         }
+
         return $this->siteName;
     }
 
     /**
-     * Sets the operators contact data
+     * @param $siteName
      *
-     * @param $operator
      * @return $this
      */
-    public function setOperator($operator) {
-        $this->operator=$operator;
+    public function setSiteName($siteName)
+    {
+        $this->siteName = $siteName;
+
         return $this;
     }
 
@@ -124,18 +128,22 @@ class ModuleOptions extends AbstractOptions {
      *
      * @return array
      */
-    public function getOperator() {
+    public function getOperator()
+    {
         return $this->operator;
     }
 
     /**
-     * Sets supported languages
+     * Sets the operators contact data
      *
-     * @param $supportedLanguages
+     * @param $operator
+     *
      * @return $this
      */
-    public function setSupportedLanguages($supportedLanguages) {
-        $this->supportedLanguages=$supportedLanguages;
+    public function setOperator($operator)
+    {
+        $this->operator = $operator;
+
         return $this;
     }
 
@@ -144,18 +152,22 @@ class ModuleOptions extends AbstractOptions {
      *
      * @return array
      */
-    public function getSupportedLanguages() {
+    public function getSupportedLanguages()
+    {
         return $this->supportedLanguages;
     }
 
     /**
-     * Sets the default language
+     * Sets supported languages
      *
-     * @param $defaultLanguage
+     * @param $supportedLanguages
+     *
      * @return $this
      */
-    public function setDefaultLanguage($defaultLanguage) {
-        $this->defaultLanguage=$defaultLanguage;
+    public function setSupportedLanguages($supportedLanguages)
+    {
+        $this->supportedLanguages = $supportedLanguages;
+
         return $this;
     }
 
@@ -164,18 +176,36 @@ class ModuleOptions extends AbstractOptions {
      *
      * @return string
      */
-    public function getDefaultLanguage() {
+    public function getDefaultLanguage()
+    {
         return $this->defaultLanguage;
+    }
+
+    /**
+     * Sets the default language
+     *
+     * @param $defaultLanguage
+     *
+     * @return $this
+     */
+    public function setDefaultLanguage($defaultLanguage)
+    {
+        $this->defaultLanguage = $defaultLanguage;
+
+        return $this;
     }
 
     /**
      * Enable or disable the detection of language setting of the browser
      *
      * @param $detectLanguage
+     *
      * @return $this
      */
-    public function setDetectLanguage($detectLanguage) {
-        $this->detectLanguage=$detectLanguage;
+    public function setDetectLanguage($detectLanguage)
+    {
+        $this->detectLanguage = $detectLanguage;
+
         return $this;
     }
 
@@ -184,19 +214,9 @@ class ModuleOptions extends AbstractOptions {
      *
      * @return bool
      */
-    public function isDetectLanguage() {
+    public function isDetectLanguage()
+    {
         return $this->detectLanguage;
-    }
-
-    /**
-     * Sets the default language
-     *
-     * @param $defaultCurrency
-     * @return $this
-     */
-    public function setDefaultCurrencyCode($defaultCurrency) {
-        $this->defaultCurrencyCode=$defaultCurrency;
-        return $this;
     }
 
     /**
@@ -204,18 +224,22 @@ class ModuleOptions extends AbstractOptions {
      *
      * @return string
      */
-    public function getDefaultCurrencyCode() {
+    public function getDefaultCurrencyCode()
+    {
         return $this->defaultCurrencyCode;
     }
 
     /**
-     * Sets the default tax rate
+     * Sets the default language
      *
-     * @param $defaultTaxRate
+     * @param $defaultCurrency
+     *
      * @return $this
      */
-    public function setDefaultTaxRate($defaultTaxRate) {
-        $this->defaultTaxRate=$defaultTaxRate;
+    public function setDefaultCurrencyCode($defaultCurrency)
+    {
+        $this->defaultCurrencyCode = $defaultCurrency;
+
         return $this;
     }
 
@@ -224,7 +248,54 @@ class ModuleOptions extends AbstractOptions {
      *
      * @return string
      */
-    public function getDefaultTaxRate() {
+    public function getDefaultTaxRate()
+    {
         return $this->defaultTaxRate;
     }
+
+    /**
+     * Sets the default tax rate
+     *
+     * @param $defaultTaxRate
+     *
+     * @return $this
+     */
+    public function setDefaultTaxRate($defaultTaxRate)
+    {
+        $this->defaultTaxRate = $defaultTaxRate;
+
+        return $this;
+    }
+
+    /**
+     * Gets system message email address.
+     *
+     * @return string
+     * @throws MissingOptionException if no value is set.
+     * @since 0.20
+     */
+    public function getSystemMessageEmail()
+    {
+        if (!$this->systemMessageEmail) {
+            throw new MissingOptionException('systemMessageEmail', $this);
+        }
+        return $this->systemMessageEmail;
+    }
+
+    /**
+     * Sets system message email address.
+     *
+     * @param string $systemMessageEmail
+     *
+     * @return self
+     * @since 0.20
+     */
+    public function setSystemMessageEmail($systemMessageEmail)
+    {
+        $this->systemMessageEmail = $systemMessageEmail;
+
+        return $this;
+    }
+
+
 }
---
+++ module/Core/test/CoreTest/Options/Exception/MissingOptionExceptionTest.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @license MIT
+ * @copyright  2013 - 2015 Cross Solution <http://cross-solution.de>
+ */
+  
+/** */
+namespace CoreTest\OptionsException;
+
+use Core\Options\Exception\MissingOptionException;
+
+/**
+ * Tests for \Core\Options\Exception\MissingOptionException
+ * 
+ * @covers \Core\Options\Exception\MissingOptionException
+ * @author Mathias Gelhausen <gelhausen@cross-solution.de>
+ * @group Core
+ * @group Core.Exception
+ * @since 0.20
+ */
+class MissingDependencyExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @testdox Extends \RuntimeException and implements \Core\Option\Exception\ExceptionInterface
+     * @coversNothing
+     */
+    public function testExtendsRuntimeException()
+    {
+        $target = new MissingOptionException('test', 'test');
+
+        $this->assertInstanceOf('\RuntimeException', $target);
+        $this->assertInstanceOf('\Core\Options\Exception\ExceptionInterface', $target);
+    }
+
+
+    public function provideConstructionTestData()
+    {
+        return array(
+            array('testKeyOne', 'testTarget', null),
+            array('testKeyTwo', new \stdClass(), null),
+            array('testKeyThree', 'testTargetTwo', new \Exception('testPreviousException'))
+        );
+    }
+    /**
+     * @testdox Allows creation by passing the missing option key and target object
+     * @dataProvider provideConstructionTestData
+     *
+     * @param string $optionKey
+     * @param string|object $object
+     * @param \Exception|null $previous
+     */
+    public function testSetsCorrectExceptionMessage($optionKey, $object, $previous)
+    {
+        $target = new MissingOptionException($optionKey, $object, $previous);
+
+        $expectedMessage = sprintf('Missing value for option "%s" in "%s"',
+                                   $optionKey, is_object($object) ? get_class($object) : $object);
+
+        $this->assertEquals($expectedMessage, $target->getMessage());
+    }
+
+    /**
+     * @testdox Provided dependency FQCN and target information are retrievable
+     */
+    public function testSetsCorrectProperties()
+    {
+        $expectedDep = 'testKey';
+        $expectedObj = 'testObj';
+
+        $target = new MissingOptionException('testKey', 'testObj');
+
+        $this->assertAttributeEquals($expectedDep, 'optionKey', $target);
+        $this->assertAttributeEquals($expectedObj, 'target', $target);
+    }
+
+
+    /**
+     * @testdox Provided dependency FQCN and target information are retrievable
+     * @dataProvider provideConstructionTestData
+     */
+    public function testGetter($optionKey, $obj, $prev)
+    {
+        $target = new MissingOptionException($optionKey, $obj);
+        $expObjFqcn = is_object($obj) ? get_class($obj) : $obj;
+
+        $this->assertEquals($optionKey, $target->getOptionKey(), 'dependency assertion failed.');
+        $this->assertEquals($obj, $target->getTarget(), 'target assertion failed.');
+        $this->assertEquals($expObjFqcn, $target->getTargetFQCN(), 'target fqcn assertion failed.');
+    }
+}
