55ed91334517d3decf718fe99d4e1a8b12572339
Carsten Bleek <bleek@cross-solution.de>
2015-08-19 15:18:35 +0200

    [Core] fixes gh-135. YAWIK does not work with PHP build-in Webserver

--- public/index.php
+++ public/index.php
@@ -17,6 +17,25 @@ date_default_timezone_set('Europe/Berlin');
  */
 chdir(dirname(__DIR__));
 
+if (php_sapi_name() == 'cli-server') {
+    $route = parse_url(substr($_SERVER["REQUEST_URI"], 1))["path"];
+    if (is_file($route)) {
+        if(substr($route, -4) == ".php"){
+            require $route;     // Include requested script files
+            exit;
+        }
+        return false;           // Serve file as is
+    } else {                    // Fallback to index.php
+        $_GET["q"] = $route;    // Try to emulate the behaviour of a .htaccess here.
+    }
+}
+
+/*
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+chdir(dirname(__DIR__));
+
 // Setup autoloading
 // Composer autoloading
 if (file_exists('vendor/autoload.php')) {
@@ -27,6 +46,6 @@ if (file_exists('vendor/autoload.php')) {
 } else {
     throw new \RuntimeException('Could not initialize autoloading.');
 }
-    
+
 // Run the application!
 Zend\Mvc\Application::init(require 'config/config.php')->run();
