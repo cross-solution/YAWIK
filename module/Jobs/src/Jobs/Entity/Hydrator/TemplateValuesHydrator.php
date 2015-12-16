7af18d4ca421e7d79b4799823c865590411e02d8
Mathias Gelhausen <gelhausen@cross-solution.de>
2015-12-16 13:46:17 +0100

    [Jobs] Adds a hydrator for the TemplateValues entity which is capable of handling FreeValues entries.

---
+++ module/Jobs/src/Jobs/Entity/Hydrator/TemplateValuesHydrator.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @license    MIT
+ * @copyright  2013 - 2015 Cross Solution <http://cross-solution.de>
+ */
+
+/** */
+namespace Jobs\Entity\Hydrator;
+
+use Core\Entity\Hydrator\EntityHydrator;
+use Jobs\Entity\TemplateValues;
+
+
+/**
+ * Hydrator handles {@link \Jobs\Entity\TemplateValues}.
+ *
+ * Additionally hydrates and extracts the FreeValues keys it is configured to care for.
+ *
+ * @author Mathias Gelhausen <gelhausen@cross-solution.de>
+ * @since  0.23
+ */
+class TemplateValuesHydrator extends EntityHydrator
+{
+    /**
+     * The FreeValues keys to care about.
+     *
+     * @var array
+     */
+    protected $freeValuesKeys = [];
+
+    /**
+     * Creates an instance.
+     *
+     * @param array|null $freeValuesKeys
+     */
+    public function __construct($freeValuesKeys = null)
+    {
+        parent::__construct();
+
+        if ($freeValuesKeys) {
+            $this->setFreeValuesKeys($freeValuesKeys);
+        }
+    }
+
+    /**
+     * Gets the FreeValues keys this instance cares about.
+     *
+     * @return array
+     */
+    public function getFreeValuesKeys()
+    {
+        return $this->freeValuesKeys;
+    }
+
+    /**
+     * Sets the FreeValues keys to care about.
+     *
+     * @param array $freeValuesKeys
+     *
+     * @return self
+     */
+    public function setFreeValuesKeys(array $freeValuesKeys)
+    {
+        $this->freeValuesKeys = $freeValuesKeys;
+
+        return $this;
+    }
+
+    /**
+     * Extract values (including the configured FreeValues) from an TemplateValues entity.
+     *
+     * @param TemplateValues $object
+     *
+     * @return array
+     */
+    public function extract($object)
+    {
+        $data = parent::extract($object);
+
+        foreach ($this->getFreeValuesKeys() as $key) {
+            $data[$key] = $object->get($key);
+        }
+
+        return $data;
+    }
+
+    /**
+     * Hydrate a TemplateValues entity with the provided $data including the configured FreeValues.
+     *
+     * @param  array          $data
+     * @param  TemplateValues $object
+     *
+     * @return TemplateValues
+     */
+    public function hydrate(array $data, $object)
+    {
+        parent::hydrate($data, $object);
+
+        foreach ($this->getFreeValuesKeys() as $key) {
+            if (isset($data[$key])) {
+                $object->set($key, $data[$key]);
+            }
+        }
+
+        return $object;
+    }
+}
---
+++ module/Jobs/test/JobsTest/Entity/Hydrator/TemplateValuesHydratorTest.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @license MIT
+ * @copyright  2013 - 2015 Cross Solution <http://cross-solution.de>
+ */
+  
+/** */
+namespace JobsTest\Entity\Hydrator;
+
+use Jobs\Entity\Hydrator\TemplateValuesHydrator;
+use Jobs\Entity\TemplateValues;
+
+/**
+ * Tests for \Jobs\Entity\Hydrator\TemplateValuesHydrator
+ * 
+ * @covers \Jobs\Entity\Hydrator\TemplateValuesHydrator
+ * @author Mathias Gelhausen <gelhausen@cross-solution.de>
+ * @group Jobs
+ * @group Jobs.Entity
+ * @group Jobs.Entity.Hydrator
+ */
+class TemplateValuesHydratorTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @testdox Extends \Core\Entity\Hydrator\EntityHydrator
+     */
+    public function testExtendsEntityHydrator()
+    {
+        $target = new TemplateValuesHydrator();
+
+        $this->assertInstanceOf('\Core\Entity\Hydrator\EntityHydrator', $target);
+    }
+
+    /**
+     * @testdox FreeValues keys can be set through the constructor.
+     */
+    public function testFreeValuesKeysCanBeSetThroughConstructor()
+    {
+        $keys = [ 'key1', 'key2' ];
+        $target = new TemplateValuesHydrator($keys);
+
+        $this->assertAttributeEquals($keys, 'freeValuesKeys', $target);
+    }
+
+    /**
+     * @testdox Allows setting and getting FreeValues keys.
+     */
+    public function testSetAndGetFreeValuesKeys()
+    {
+        $target = new TemplateValuesHydrator();
+        $keys = [ 'key1', 'key2' ];
+
+        $this->assertSame($target, $target->setFreeValuesKeys($keys), 'Fluent interface broken.');
+        $this->assertEquals($keys, $target->getFreeValuesKeys());
+    }
+
+    /**
+     * @testdox Extracts a TemplateValues object with the set FreeValues keys.
+     */
+    public function testExtract()
+    {
+        $values = new TemplateValues();
+        $values->freeKey1 = 'value1';
+        $values->ignoredKey = 'irrelephant';
+
+        $target = new TemplateValuesHydrator([ 'freeKey1' ]);
+
+        $data = $target->extract($values);
+
+        $this->assertArrayHasKey('freeKey1', $data);
+        $this->assertEquals('value1', $data['freeKey1']);
+        $this->assertArrayNotHasKey('ignoredKey', $data);
+    }
+
+    /**
+     * @testdox Hydrates data to a TemplateValues object including the configured FreeValues.
+     */
+    public function testHydrate()
+    {
+        $values = new TemplateValues();
+        $target = new TemplateValuesHydrator([ 'testKeyOne' ]);
+        $data = [
+            'testKeyOne' => 'testValue',
+            'ignoredKey' => 'irrelevant'
+        ];
+
+        $this->assertSame($values, $target->hydrate($data, $values), 'hydrate() does not return object.');
+        $this->assertEquals('testValue', $values->get('testKeyOne'));
+        $this->assertNull($values->get('ignoredKey'));
+
+    }
+}
