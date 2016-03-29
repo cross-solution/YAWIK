a791bfbbc88eb228bbc6737a2cfb0bac070d10d1
Carsten Bleek <bleek@cross-solution.de>
2016-03-29 12:28:03 +0200

    [Auth] modifies navigation. Moves Users and Orders into Admin section

--- module/Auth/config/navigation.config.php
+++ module/Auth/config/navigation.config.php
@@ -4,17 +4,20 @@
  *
  * @filesource
  * @copyright (c) 2013-2016 Cross Solution (http://cross-solution.de)
- * @author cbleek
- * @license   MIT
+ * @author        cbleek
+ * @license       MIT
  */
 
-return ['navigation' => [
-    'default' => [
-        'admin' => [
-            'label' =>  /*@translate*/ 'User',
-            'route' => 'lang/user-list',
-            'order' => '100',
-            'resource' => 'Users',
-        ],
+return [
+    'navigation' => [
+        'default' => [
+            'admin' => [
+                'label'    =>  /*@translate*/ 'Users',
+                'route'    => 'lang/user-list',
+                'order'    => '100',
+                'resource' => 'Users',
+
+            ],
+        ]
     ]
-]];
+];
--- module/Orders/config/navigation.config.php
+++ module/Orders/config/navigation.config.php
@@ -3,16 +3,24 @@
  * YAWIK
  *
  * @filesource
- * @license MIT
+ * @license    MIT
  * @copyright  2013 - 2016 Cross Solution <http://cross-solution.de>
  */
 
-return [ 'navigation' => [ 'default' => [
-
-    'orders' => [
-        'label' =>  /*@translate*/ 'Orders',
-        'route' => 'lang/orders',
-        //'order' => '30',
-        'resource' => 'Orders/Navigation',
-    ],
-]]];
+return [
+    'navigation' => [
+        'default' => [
+            'admin' => [
+                'label' => 'Admin',
+                'pages' => [
+                    'orders' => [
+                        'label'    =>  /*@translate*/ 'Orders',
+                        'route'    => 'lang/orders',
+                        'order'    => '100',
+                        'resource' => 'Orders/Navigation',
+                    ],
+                ]
+            ]
+        ]
+    ]
+];
