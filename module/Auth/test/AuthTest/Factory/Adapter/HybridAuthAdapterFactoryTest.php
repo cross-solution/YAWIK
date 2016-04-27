91c441b338611e414e1a9191e86ad1e4cde60d8d
fedys <miroslav.fedeles@gmail.com>
2016-04-27 16:24:43 +0200

    fixed "Zend\ServiceManager\ServiceManager::get('ControllerPluginManager', true) was not expected to be called more than 2 times"

--- module/Auth/test/AuthTest/Factory/Adapter/HybridAuthAdapterFactoryTest.php
+++ module/Auth/test/AuthTest/Factory/Adapter/HybridAuthAdapterFactoryTest.php
@@ -46,15 +46,29 @@ class HybridAuthAdapterFactoryTest extends \PHPUnit_Framework_TestCase
             ->method('get')
             ->with('Auth/User')
             ->willReturn($userRepositoryMock);
+        
+        $socialProfilesPluginMock = $this->getMockBuilder('Auth\Controller\Plugin\SocialProfiles')
+            ->disableOriginalConstructor()
+            ->getMock();
+        
+        $contollerPluginsMock = $this->getMockBuilder('Zend\Mvc\Controller\PluginManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        
+        $contollerPluginsMock->expects($this->once())
+            ->method('get')
+            ->with('Auth/SocialProfiles')
+            ->willReturn($socialProfilesPluginMock);
 
         $sm = $this->getMockBuilder('\Zend\ServiceManager\ServiceManager')->disableOriginalConstructor()->getMock();
 
-        $sm->expects($this->exactly(2))
+        $sm->expects($this->exactly(3))
             ->method('get')
             ->will($this->returnValueMap(
                 array(
                     array('HybridAuth', true, $hybridAuthMock),
-                    array('repositories', true, $repositoriesMock)
+                    array('repositories', true, $repositoriesMock),
+                    array('ControllerPluginManager', true, $contollerPluginsMock)
                 )
             ));
 
