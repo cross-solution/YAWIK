9e75b15857df45044da5db18b41c57f6b6f17fce
Mathias Gelhausen <gelhausen@cross-solution.de>
2016-05-06 16:11:24 +0200

    [Core] Adds unit tests for TextSearchForm and its fieldsets.

---
+++ module/Core/test/CoreTest/Form/TextSearchFormFieldsetTest.php
@@ -0,0 +1,203 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @license MIT
+ * @copyright  2013 - 2016 Cross Solution <http://cross-solution.de>
+ */
+  
+/** */
+namespace CoreTest\Form;
+
+use Core\Form\TextSearchFormButtonsFieldset;
+use Core\Form\TextSearchFormFieldset;
+use CoreTestUtils\TestCase\AssertInheritanceTrait;
+use CoreTestUtils\TestCase\SetterGetterTrait;
+use Zend\Form\Element\Text;
+use Zend\Form\Fieldset;
+
+/**
+ * Tests for \Core\Form\TextSearchFormFieldset
+ * 
+ * @covers \Core\Form\TextSearchFormFieldset
+ * @author Mathias Gelhausen <gelhausen@cross-solution.de>
+ * @group Core
+ * @group Core.Form
+ */
+class TextSearchFormFieldsetTest extends \PHPUnit_Framework_TestCase
+{
+    use AssertInheritanceTrait, SetterGetterTrait;
+
+    /**
+     *
+     *
+     * @var string|\Core\Form\TextSearchForm
+     */
+    protected $target = [
+        'class' => '\Core\Form\TextSearchFormFieldset',
+        'mock' => [
+            'testPassingTextElementOptionsWithNoTextElementSet' => ['has', 'get'],
+            'testPassingTextElementOptionsWithTextElementSet' => ['has', 'get'],
+            'testAddsTextElementOnInitialization' => ['add'],
+        ]
+    ];
+
+    protected $inheritance = [ '\Zend\Form\Fieldset'  ];
+
+    public function propertiesProvider()
+    {
+        return [
+            [ 'buttonElement', 'test' ],
+            [ 'buttonElement', [
+                'ignore_setter' => true,
+                'value' => 'text',
+                'post' => ['assertEquals', [ 'text', ['->getOption', ['button_element']], 'Failed']],
+            ]],
+            [ 'columnMap', [ 'value' => [1,2,3]] ],
+            [ 'columnMap', [
+                'value' => ['text' => 5 ],
+                'ignore_setter' => true,
+                'pre' => 'populateElementsForColumnMapTest',
+                'post' => ['assertEquals', [ ['text' => 5], ['->getOption', ['column_map']]]],
+            ]]
+
+        ];
+    }
+
+    public function populateElementsForColumnMapTest()
+    {
+        $text = new Text();
+        $text->setName('text');
+        $text->setOption('span', 5);
+
+        $dummy = new Text();
+        $dummy->setName('dummy');
+
+        $this->target->add($text);
+        $this->target->add($dummy);
+    }
+
+    public function testPassingTextElementOptionsWithNoTextElementSet()
+    {
+        $this->target->expects($this->once())->method('has')->with('text')->willReturn(false);
+        $this->target->expects($this->never())->method('get');
+        $this->target->setOptions([
+                                'text_placeholder' => 'placeholder',
+                                'text_span' => 10,
+                                'text_label' => 'Test'
+                            ]);
+
+    }
+
+    public function testPassingTextElementOptionsWithTextElementSet()
+    {
+        $placeholder = 'placeholder';
+        $span = 8;
+        $label = 'Test';
+
+        $text = $this->getMock('\Zend\Form\Element\Text', ['setAttribute', 'setOption', 'setLabel']);
+        $text->expects($this->once())->method('setAttribute')->with('placeholder', $placeholder);
+        $text->expects($this->once())->method('setOption')->with('span', $span);
+        $text->expects($this->once())->method('setLabel')->with($label);
+
+
+        $this->target->expects($this->once())->method('has')->with('text')->willReturn(true);
+        $this->target->expects($this->once())->method('get')->with('text')->willReturn($text);
+
+        $this->target->setOptions([
+                                'text_placeholder' => $placeholder,
+                                'text_span' => $span,
+                                'text_label' => $label
+                            ]);
+    }
+
+
+    public function testAddsTextElementOnInitialization()
+    {
+        $label = 'Test';
+        $placeholder = 'Placeholder';
+        $span = 8;
+
+        $expect1 =
+            [
+                'type' => 'Text',
+                'name' => 'text',
+                'options' => [
+                    'label' => 'Search',
+                    'span' => 12,
+                ],
+                'attributes' => [
+                    'placeholder' => 'Search query',
+                ],
+            ];
+
+        $expect2 =
+            [
+                'type' => 'Text',
+                'name' => 'text',
+                'options' => [
+                    'label' => $label,
+                    'span' => $span,
+                ],
+                'attributes' => [
+                    'placeholder' => $placeholder,
+                ],
+            ];
+
+
+
+        $this->target->expects($this->exactly(2))->method('add')
+            ->withConsecutive(
+                [ $expect1 ],
+                [ $expect2 ]
+            );
+
+        $this->target->init();
+
+        $this->target->setOptions([
+                                      'text_label' => $label,
+                                      'text_placeholder'=> $placeholder,
+                                      'text_span' => $span
+        ]);
+
+        $this->target->init();
+
+    }
+
+    public function testAddElementViaArraySpec()
+    {
+        $this->target->add([
+                               'type' => 'Text',
+                               'name' => 'Test',
+
+                           ]);
+
+        $element = $this->target->get('Test');
+        $this->assertContains('form-control', $element->getAttribute('class'));
+
+        $this->target->add([
+                               'type' => 'Text',
+                               'name' => 'Test2',
+                               'options' => [
+                                'is_button_element' => true,
+                                ]
+                           ]);
+
+        $this->assertEquals('Test2', $this->target->getButtonElement());
+    }
+
+    public function testAddElementViaObject()
+    {
+        $test = new Text('test');
+
+        $this->target->add($test);
+        $this->assertContains('form-control', $test->getAttribute('class'));
+
+        $test2 = new Text('test2', ['is_button_element' => true]);
+
+        $this->target->add($test2);
+        $this->assertEquals('test2', $this->target->getButtonElement());
+    }
+
+}
--- module/Core/test/CoreTest/Form/TextSearchFormTest.php
+++ module/Core/test/CoreTest/Form/TextSearchFormTest.php
@@ -10,8 +10,11 @@
 /** */
 namespace CoreTest\Form;
 
+use Core\Form\TextSearchFormButtonsFieldset;
+use Core\Form\TextSearchFormFieldset;
 use CoreTestUtils\TestCase\AssertInheritanceTrait;
 use CoreTestUtils\TestCase\SetterGetterTrait;
+use Zend\Form\Fieldset;
 
 /**
  * Tests for \Core\Form\TextSearchForm
@@ -32,54 +35,212 @@ class TextSearchFormTest extends \PHPUnit_Framework_TestCase
      */
     protected $target = '\Core\Form\TextSearchForm';
 
-    protected $inheritance = [ '\Zend\Form\Form' , '\Core\Form\HeadscriptProviderInterface' ];
+    protected $inheritance = [ '\Zend\Form\Form'  ];
 
-    protected $properties = [
-        [ 'Headscripts', [ 'default' => [ 'Core/js/core.searchform.js' ], 'value' => [ 'some/other/headscripts' ] ] ],
-    ];
+    public function propertiesProvider()
+    {
+        return [
+            [ 'options', [
+                'value' => [],
+                'ignore_getter' => true,
+                'setter_assert' => 'assertSetOptionsResult',
+                'setter_value' => 'empty',
+            ]],
+            [ 'options', [
+                'value' => ['elements_fieldset' => 'Test'],
+                'ignore_getter' => true,
+                'setter_assert' => 'assertSetOptionsResult',
+                'setter_value' => 'elements',
+            ]],
+            [ 'options', [
+                'value' => ['buttons_fieldset' => 'Test'],
+                'ignore_getter' => true,
+                'setter_assert' => 'assertSetOptionsResult',
+                'setter_value' => 'buttons',
+            ]],
+            [ 'options', [
+                'value' => ['name' => 'test'],
+                'ignore_getter' => true,
+                'setter_assert' => 'assertSetNameViaOptions',
+                'setter_value' => 'test'
+            ]],
+        ];
+    }
+
+    public function assertSetOptionsResult($name, $returned, $expect)
+    {
+        $this->assertSame($returned, $this->target);
+
+        $expectOptions = [];
+        $expectElementsFieldset = 'Core/TextSearch/Elements';
+        $expectButtonsFieldset  = 'Core/TextSearch/Buttons';
+
+
+        if ('elements' == $expect) {
+            $expectOptions = [ 'elements_fieldset' => 'Test' ];
+            $expectElementsFieldset = 'Test';
+        } else if ('buttons' == $expect) {
+            $expectOptions = [ 'buttons_fieldset' => 'Test' ];
+            $expectButtonsFieldset = 'Test';
+        }
+
+        $this->assertAttributeEquals($expectOptions, 'options', $this->target);
+        $this->assertAttributeEquals($expectElementsFieldset, 'elementsFieldset', $this->target);
+        $this->assertAttributeEquals($expectButtonsFieldset, 'buttonsFieldset', $this->target);
+
+    }
+
+    public function assertSetNameViaOptions($name, $returned, $expect)
+    {
+        $this->assertEquals($this->target->getName(), $expect);
+    }
 
     public function testAllowsSettingSearchParamsAsJson()
     {
         $arrayValue = [ 'test' => 'works' ];
         $arrayExpect = \Zend\Json\Json::encode($arrayValue);
 
+        $traversableValue = new \ArrayIterator($arrayValue);
+        $traversableExpect = \Zend\Json\Json::encode($arrayValue);
 
 
         $this->assertSame($this->target, $this->target->setSearchParams($arrayValue), 'breaks fluent interface.');
         $this->assertEquals($arrayExpect, $this->target->getAttribute('data-search-params'), 'Setting array failed.');
 
+        $this->target->setSearchParams($traversableValue);
+        $this->assertEquals($traversableExpect, $this->target->getAttribute('data-search-params'), 'Setting \Traversable failed.');
+
 
     }
 
-    public function testInitializesItself()
+    public function testInitializesItselfSetsDefaultAttributes()
     {
-        /* @var \Core\Form\TextSearchForm|\PHPUnit_Framework_MockObject_MockObject $target */
-        $name = 'search';
-        $placeholder = 'placeholder';
         $attributes = [
-            'data-handle-by' => 'native',
-            'method' => 'get',
             'class' => 'form-inline search-form',
+            'data-handle-by' => 'script',
+            'method' => 'get',
         ];
-        $add = [
-                       'type' => 'Text',
-                       'name' => 'text',
-                       'options' => [
-                           'label' => 'Search',
-                           'use_formrow_helper' => false,
-                       ],
-                       'attributes' => [
-                           'class' => 'form-control',
-                           'placeholder' => $placeholder,
-                       ]
-                   ];
-
-        $target = $this->getMock(get_class($this->target), ['add', 'setName', 'setAttributes', 'getOption']);
-        $target->expects($this->once())->method('setName')->with($name);
+        $target = $this->getMock(get_class($this->target), ['add', 'setAttributes']);
         $target->expects($this->once())->method('setAttributes')->with($attributes);
-        $target->expects($this->once())->method('getOption')->with('placeholder')->willReturn($placeholder);
-        $target->expects($this->once())->method('add')->with($add);
 
         $target->init();
+    }
+
+    public function testInitializesItselfSetsDefaultName()
+    {
+        $target = $this->getMock(get_class($this->target), ['add', 'setName']);
+        $target->expects($this->once())->method('setName')->with('search');
+
+        $target->init();
+    }
+
+    public function testInitializesItselfUsesName()
+    {
+        $target = $this->getMock(get_class($this->target), ['add', 'setName']);
+        $target->expects($this->never())->method('setName');
+        $target->setAttribute('name', 'test');
+        $target->init();
+    }
+
+    public function testInitializesItselfWithNonObjectFieldsets()
+    {
+        $target = $this->getMock(get_class($this->target), ['add']);
+
+        $addElements = [
+            'type' => 'Core/TextSearch/Elements',
+            'options' => []
+        ];
+
+        $addButtons = [
+            'type' => 'Core/TextSearch/Buttons',
+        ];
+
+        $target->expects($this->exactly(2))
+            ->method('add')->withConsecutive([$addElements], [$addButtons]);
+
+        $target->init();
+    }
+
+    public function testInitializesItselfWithObjectFieldset()
+    {
+        $target  = $this->getMock(get_class($this->target), ['add']);
+        $elements = new TextSearchFormFieldset();
+        $buttons  = new TextSearchFormButtonsFieldset();
+
+        $options = [
+            'elements_fieldset' => $elements,
+            'buttons_fieldset' => $buttons,
+        ];
+
+        $target
+            ->expects($this->exactly(2))
+            ->method('add')
+            ->withConsecutive([$elements], [$buttons]);
+
+        $target->setOptions($options);
+        $target->init();
+    }
+
+    public function invalidNamedElementsProvider()
+    {
+        $fs = new TextSearchFormButtonsFieldset();
+        $fs->setName('invalid');
+
+        return [
+            [ 'invalidnamed', [] ],
+            [ ['type' => 'Core/TextSearch/Elements'], ['name' => 'invalid']],
+            [ ['type' => 'Core/TextSearch/Elements', 'name' => 'invalid'], []],
+            [ new TextSearchFormFieldset(), []],
+            [ $fs, []]
+        ];
+    }
+
+    /**
+     * @dataProvider invalidNamedElementsProvider
+     *
+     * @param $element
+     * @param $flags
+     */
+    public function testAddElementsWithInvalidNameThrowsException($element, $flags)
+    {
+        $this->setExpectedException('\InvalidArgumentException', 'Invalid named element');
+
+        $this->target->add($element, $flags);
+    }
+
+    public function testAddInvalidElementsThrowsException()
+    {
+        $this->setExpectedException('\UnexpectedValueException', 'extends from TextSearchForm');
+
+        $this->target->add(new Fieldset(), ['name' => 'elements']);
+    }
+
+    public function testAddValidElements()
+    {
+        $elements = new TextSearchFormFieldset();
+
+        $this->target->add($elements, ['name' => 'elements']);
+
+        $actual = $this->target->get('elements');
+
+            $this->assertSame($elements, $actual);
+    }
+
+    public function testGetElementsAndGetButtonsProxyToGet()
+    {
+        $elements = new TextSearchFormFieldset();
+        $buttons = new TextSearchFormButtonsFieldset();
+        $target = $this->getMock(get_class($this->target), ['get']);
+
+        $target
+            ->expects($this->exactly(4))
+            ->method('get')
+            ->withConsecutive(['elements'], ['buttons'], ['elements'], ['buttons'])
+            ->will($this->onConsecutiveCalls($elements, $buttons, $elements, $buttons));
+        $target->add($elements, ['name' => 'elements']);
+        $target->add($buttons, ['name' => 'buttons']);
+
+        $target->getElements();
+        $target->getButtons();
     }
 }
