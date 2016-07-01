64656f6c4f11953a6624c385344429b658299abd
Anthonius Munthi <me@itstoni.com>
2016-07-01 14:08:12 +0800

    [Solr] added ServerException class to handle Solr Server related exception

---
+++ module/Solr/src/Solr/Exception/ServerException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @copyright (c) 2013 - 2016 Cross Solution (http://cross-solution.de)
+ * @license   MIT
+ */
+
+namespace Solr\Exception;
+
+
+use Core\Exception\ExceptionInterface;
+
+class ServerException extends \RuntimeException implements ExceptionInterface
+{
+}
