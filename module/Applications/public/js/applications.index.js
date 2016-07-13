cb59b8fa0a82cea8843a50885700613ba293e876
fedys <miroslav.fedeles@gmail.com>
2016-07-13 13:36:25 +0200

    refactoring multiple move action refs gh-233

--- module/Applications/public/js/applications.index.js
+++ module/Applications/public/js/applications.index.js
@@ -70,3 +70,20 @@
 	});
 	
 })(jQuery);
+
+$(document).on('click', '#application-multiple-move', function (event) {
+	event.preventDefault();
+	$(event.target).trigger('wait.stop');
+	var ids = getTableMultiCheckbox(event.target);
+	if (ids.length == 0) {
+		return;
+	}
+	var $modal = $('#application-multiple-move-modal'),
+		$form = $modal.find('form');
+	
+	$form.find('input[type=hidden]').remove();
+	$.each(ids, function (index, id){
+		$form.append('<input type="hidden" name="ids[]" value="' + id + '" >');
+	})
+	$modal.modal();
+});
--- module/Cv/src/Cv/Repository/Cv.php
+++ module/Cv/src/Cv/Repository/Cv.php
@@ -2,12 +2,15 @@
 
 namespace Cv\Repository;
 
-use Zend\ServiceManager\ServiceLocatorInterface;
 use Core\Repository\AbstractRepository;
 use Auth\Entity\UserInterface;
+use Applications\Entity\Application;
+use Cv\Entity\Cv as CvEntity;
 
 /**
  * class for accessing CVs
+ *
+ * @method CvEntity create(array $data = null, $persist = false)
  */
 class Cv extends AbstractRepository
 {
@@ -15,7 +18,7 @@ class Cv extends AbstractRepository
      * Look for an drafted Document of a given user
      *
      * @param $user
-     * @return \Cv\Entity\Cv|null
+     * @return CvEntity|null
      */
     public function findDraft($user)
     {
@@ -35,5 +38,53 @@ class Cv extends AbstractRepository
         }
 
         return null;
+    }
+    
+    /**
+     * @param Application $application
+     * @param UserInterface $user
+     * @return CvEntity
+     * @since 0.26
+     */
+    public function createFromApplication(Application $application, UserInterface $user)
+    {
+        $cv = $this->create();
+        $contact = $application->getContact();
+        $contactImage = $contact->getImage();
+        
+        if ($contactImage)
+        {
+            $contactImage->setUser($user);
+        }
+        
+        $cv->setContact($contact);
+        
+        $applicationAttachments = $application->getAttachments();
+        
+        if (count($applicationAttachments) > 0)
+        {
+            $cvAttachments = [];
+        
+            /* @var $applicationAttachment \Applications\Entity\Attachment */
+            foreach ($applicationAttachments as $applicationAttachment)
+            {
+                $file = new \Doctrine\MongoDB\GridFSFile();
+                $file->setBytes($applicationAttachment->getContent());
+                
+                $cvAttachment = new \Cv\Entity\Attachment();
+                $cvAttachment->setName($applicationAttachment->getName());
+                $cvAttachment->setType($applicationAttachment->getType());
+                $cvAttachment->setPermissions($cvAttachment->getPermissions());
+                $cvAttachment->setUser($user);
+                $cvAttachment->setFile($file);
+                $cvAttachment->setDateUploaded($applicationAttachment->getDateUploaded());
+                
+                $cvAttachments[] = $cvAttachment;
+            }
+            
+            $cv->setAttachments(new \Doctrine\Common\Collections\ArrayCollection($cvAttachments));
+        }
+        
+        return $cv;
     }
 }
