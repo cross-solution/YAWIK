521bdccec13a9a4465e0d819c0743d4b442b20ff
Mathias Gelhausen <gelhausen@cross-solution.de>
2017-01-03 19:07:29 +0100

    [Jobs] Add config file for default 'Professions' category

---
+++ module/Jobs/config/jobs.categories.professions.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @license MIT
+ * @copyright  2013 - 2017 Cross Solution <http://cross-solution.de>
+ */
+
+/*
+ * Format:
+ * [
+ *      'name' => <name>, [required]
+ *      'value' => <value>, [optional]
+ *      'children' => [ // optional
+ *          <name> // strings will be treated as ['name' => <name>]
+ *          [
+ *              'name' => <name>, [required]
+ *              'value' => <value>, [optional]
+ *              'children' => [ ... ]
+ *       ]
+ * ]
+ */
+
+return [
+    'name' => 'Professions',
+];
+
