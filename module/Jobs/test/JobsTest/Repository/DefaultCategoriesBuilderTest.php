bd032ce6ef283ee3cc5901857442576198e67df0
Mathias Gelhausen <gelhausen@cross-solution.de>
2017-01-03 19:06:48 +0100

    [Jobs] Unit tests for DefaultCategoriesBuilder

---
+++ module/Jobs/test/JobsTest/Repository/DefaultCategoriesBuilderTest.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @license MIT
+ * @copyright  2013 - 2017 Cross Solution <http://cross-solution.de>
+ */
+  
+/** */
+namespace JobsTest\Repository;
+
+use CoreTestUtils\TestCase\SetupTargetTrait;
+use Jobs\Entity\Category;
+use Jobs\Repository\DefaultCategoriesBuilder;
+use org\bovigo\vfs\vfsStream;
+
+/**
+ * Tests for \Jobs\Repository\DefaultCategoriesBuilder
+ * 
+ * @covers \Jobs\Repository\DefaultCategoriesBuilder
+ * @author Mathias Gelhausen <gelhausen@cross-solution.de>
+ * @group Jobs
+ * @group Jobs.Repository
+ */
+class DefaultCategoriesBuilderTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testBuildLoadsConfigFromGlobalPath()
+    {
+
+
+        vfsStream::setup('/');
+        vfsStream::create(['config' => ['autoload' => ['jobs.categories.professions.php' => '<?php return ["name"=>"works"];']]]);
+        $target = new DefaultCategoriesBuilder('.', [vfsStream::url('config/autoload/')], new Category());
+
+        $category = $target->build('professions');
+
+        $this->assertEquals('works', $category->getName());
+
+    }
+
+    public function testBuildLoadsConfigFromModulePath()
+    {
+
+
+        vfsStream::setup('/');
+        vfsStream::create(['module' => ['Jobs' => ['config' => ['jobs.categories.professions.php' => '<?php return ["name"=>"works"];']]]]);
+        $target = new DefaultCategoriesBuilder(vfsStream::url('./module/Jobs/config/'), [], new Category());
+
+        $category = $target->build('professions');
+
+        $this->assertEquals('works', $category->getName());
+
+    }
+
+    public function testBuildCreatesEmptyCategory()
+    {
+
+
+        vfsStream::setup('/');
+
+        $target = new DefaultCategoriesBuilder(vfsStream::url('./module'), [], new Category());
+
+        $category = $target->build('professions');
+
+        $this->assertEquals('professions', $category->getName());
+
+    }
+
+    public function testBuildCreatesHirarchy()
+    {
+
+
+        vfsStream::setup('/');
+        vfsStream::create(['config' => ['autoload' => ['jobs.categories.professions.php' => '<?php return ["name"=>"works", "children" => ["string", ["name" => "array"]]];']]]);
+        $target = new DefaultCategoriesBuilder('.', [vfsStream::url('config/autoload/')], new Category());
+
+        $category = $target->build('professions');
+
+        $this->assertEquals('works', $category->getName());
+        $children = $category->getChildren();
+        $this->assertEquals(2, $children->count());
+        $this->assertEquals('string', $children->first()->getName());
+        $this->assertEquals('array', $children->last()->getName());
+
+    }
+
+
+}
