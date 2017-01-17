ab675afc75be074e1fbf299bc05d66802916932e
Mathias Gelhausen <gelhausen@cross-solution.de>
2017-01-17 10:09:39 +0100

    [Jobs] Replaces location field with new GeoSelect in Jobboard SearchForm.

---
+++ module/Jobs/src/Jobs/Form/JobboardSearch.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @copyright (c) 2013 - 2016 Cross Solution (http://cross-solution.de)
+ * @license       MIT
+ */
+
+/** ListFilterLocationFieldset.php */
+namespace Jobs\Form;
+
+use Core\Form\SearchForm;
+use Core\Form\TextSearchFormFieldset;
+
+/**
+ * Adds the location search to the base search form.
+ *
+ * @package Jobs\Form
+ */
+class JobboardSearch extends SearchForm
+{
+
+    /**
+     * @var
+     */
+    protected $locationEngineType;
+
+    /**
+     * @param array $options
+     */
+    public function __construct(array $options = [])
+    {
+        parent::__construct();
+        if (array_key_exists('location_engine_type', $options)) {
+            $this->locationEngineType = $options['location_engine_type'];
+        }
+        $this->setOptions($options);
+    }
+
+    public function init()
+    {
+        $this->setOption('text_span', 5);
+        parent::init();
+        $this->setButtonElement('d');
+
+
+//        $this->add(
+//            array(
+//                'name'    => 'l',
+//                'type'    => 'Location',
+//                'options' => array(
+//                    'label'       => /*@translate*/ 'Location',
+//                    'engine_type' => $this->locationEngineType,
+//                    'span' => 3,
+//                ),
+//                'attributes' => [
+//                    'placeholder' => 'Location',
+//                ],
+//            )
+//        );
+//
+//        $this->get('l')->setViewPartial('geo/form/GeoHorizontal');
+
+        $this->add([
+                'name' => 'l',
+                'type' => 'LocationSelect',
+                'options' => [
+                    'label' => 'Location',
+                    'span' => 3
+                ],
+                'attributes' => [
+                    'data-width' => '100%',
+                ]
+            ]);
+
+        $this->add(
+            array(
+                'name'    => 'd',
+                'type'    => 'Zend\Form\Element\Select',
+                'options' => array(
+                    'label'         => /*@translate*/ 'Distance',
+                    'value_options' => [
+                        '5'   => '5 km',
+                        '10'  => '10 km',
+                        '20'  => '20 km',
+                        '50'  => '50 km',
+                        '100' => '100 km'
+                    ],
+                    'span' => 4,
+
+                ),
+                'attributes' => [
+                    'value' => '10', // default distance
+                    'data-searchbox'  => -1,  // hide the search box
+                    'data-allowclear' => 'false', // allow to clear a selected value
+                    'data-placeholder'  => /*@translate*/ 'Distance',
+                    'data-width' => '100%',
+                ]
+            )
+        );
+    }
+}
