a650ada216ccdae8774a62dae6241dd2128e2d4d
Carsten Bleek <bleek@cross-solution.de>
2017-02-10 10:22:25 +0100

    [General] copies js (jquery, bootstrap,...) assets to public/assets

---
+++ bin/install-assets.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# will be executed by npm install as "prepublish" script
+#
+
+ASSETS_DIR=public/assets/
+
+if [ ! -d "${ASSETS_DIR}" ]; then
+	 mkdir -p ${ASSETS_DIR}
+fi
+
+cp -R node_modules/jquery/dist ${ASSETS_DIR}/jquery
+cp -R node_modules/bootstrap3-dialog/dist ${ASSETS_DIR}/bootstrap3-dialog
+cp -R node_modules/bootstrap-datepicker/dist ${ASSETS_DIR}/bootstrap-datepicker
+cp -R node_modules/select2/dist ${ASSETS_DIR}/select2
+cp -R node_modules/blueimp-file-upload ${ASSETS_DIR}/blueimp-file-upload
+cp -R node_modules/pnotify ${ASSETS_DIR}/pnotify
