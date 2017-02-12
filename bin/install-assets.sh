580d5474f0a52467e6f65b9c305074d4335e8794
Carsten Bleek <bleek@cross-solution.de>
2017-02-12 17:19:40 +0100

    [Core] moves pnotify to public/assets

--- bin/install-assets.sh
+++ bin/install-assets.sh
@@ -15,6 +15,14 @@ cp -R node_modules/bootstrap3-dialog/dist ${ASSETS_DIR}/bootstrap3-dialog
 cp -R node_modules/bootstrap-datepicker/dist ${ASSETS_DIR}/bootstrap-datepicker
 cp -R node_modules/select2/dist ${ASSETS_DIR}/select2
 cp -R node_modules/blueimp-file-upload ${ASSETS_DIR}/blueimp-file-upload
+
+
+uglifyjs node_modules/pnotify/dist/pnotify.js \
+	node_modules/pnotify/dist/pnotify.buttons.js \
+	-o public/assets/pnotify/pnotify.custom.min.js
+	
+cat node_modules/pnotify/dist/pnotify.css node_modules/pnotify/dist/pnotify.buttons.css > ${ASSETS_DIR}/pnotify/pnotify.custom.min.css	
+
 cp -R node_modules/pnotify/dist ${ASSETS_DIR}/pnotify
 cp -R node_modules/bootstrap/dist ${ASSETS_DIR}/bootstrap
 
@@ -22,4 +30,5 @@ if [ ! -d "${ASSETS_DIR}/twitter-bootstrap-wizard" ]; then
          mkdir -p ${ASSETS_DIR}/twitter-bootstrap-wizard
 fi
 cp -R node_modules/twitter-bootstrap-wizard/jquery.bootstrap.wizard.min.js ${ASSETS_DIR}/twitter-bootstrap-wizard/jquery.bootstrap.wizard.min.js
+
 cp -R node_modules/twitter-bootstrap-wizard/jquery.bootstrap.wizard.js ${ASSETS_DIR}/twitter-bootstrap-wizard/jquery.bootstrap.wizard.js
