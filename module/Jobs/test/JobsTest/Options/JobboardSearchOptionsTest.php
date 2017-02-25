1b215b842e555a7336efb93a772462ace43b7c69
Carsten Bleek <bleek@cross-solution.de>
2017-02-25 17:07:54 +0100

    unitest for JobboardSearchOptions

---
+++ module/Jobs/test/JobsTest/Options/JobboardSearchOptionsTest.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @copyright (c) 2013 - 2016 Cross Solution (http://cross-solution.de)
+ * @license   MIT
+ */
+
+namespace SolrTest\Options;
+
+use CoreTestUtils\TestCase\TestSetterGetterTrait;
+use CoreTestUtils\TestCase\SetupTargetTrait;
+use Jobs\Options\JobboardSearchOptions;
+
+/**
+ * Class ModuleOptionsTest
+ *
+ * @author  Carsten Bleek <bleek@cross-solution.de>
+ * @since   0.27
+ * @covers  Jobs\Options\JobboardSearchOptions
+ * @package JobsTest\Options
+ */
+class JobboardSearchOptionsTest extends \PHPUnit_Framework_TestCase
+{
+    use TestSetterGetterTrait, SetupTargetTrait;
+
+    protected $target = [
+        'class' => JobboardSearchOptions::class
+    ];
+
+    public function propertiesProvider()
+    {
+        return [
+            ['fields', [
+                'value' => [
+                    'q' => [
+                        'enabled' => false
+                    ],
+                    'l' => [
+                        'enabled' => true
+                    ],
+                    'd' => [
+                        'enabled' => true
+                    ],
+                    'c' => [
+                        'enabled' => true
+                    ],
+                    't' => [
+                        'enabled' => true,
+                    ]
+                ],
+                'default' => [
+                    'q' => [
+                        'enabled' => true
+                    ],
+                    'l' => [
+                        'enabled' => true
+                    ],
+                    'd' => [
+                        'enabled' => true
+                    ],
+                    'c' => [
+                        'enabled' => true
+                    ],
+                    't' => [
+                        'enabled' => true,
+                    ]
+                ]
+            ]],
+            ['perPage', [
+                'value' => 20,
+                'default' => 10
+            ]],
+        ]
+        ;
+    }
+}
