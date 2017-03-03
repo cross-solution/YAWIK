ffcf0629e520823d2b4f1f3b97b8d0030d2d398d
Mathias Gelhausen <gelhausen@cross-solution.de>
2017-03-03 16:29:17 +0100

    [Core] Unit tests for new Status entities

---
+++ module/Core/test/CoreTest/Entity/Status/AbstractSortableStatusTest.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @license MIT
+ * @copyright  2013 - 2017 Cross Solution <http://cross-solution.de>
+ */
+  
+/** */
+namespace CoreTest\Entity\Status;
+
+use Core\Entity\Status\AbstractSortableStatus;
+use Core\Entity\Status\AbstractStatus;
+use CoreTestUtils\TestCase\TestInheritanceTrait;
+
+/**
+ * Tests for \Core\Entity\Status\AbstractSortableStatus
+ * 
+ * @covers \Core\Entity\Status\AbstractSortableStatus
+ * @author Mathias Gelhausen <gelhausen@cross-solution.de>
+ * @group Core
+ * @group Core.Entity
+ * @group Core.Entity.Status
+ */
+class AbstractSortableStatusTest extends \PHPUnit_Framework_TestCase
+{
+    use TestInheritanceTrait;
+
+    private $target = [
+        false,
+        '@testInheritance' => [
+            AbstractSortableStatus::class,
+            'as_reflection' => true,
+        ],
+    ];
+
+    private $inheritance = [ AbstractStatus::class ];
+
+    public function xtestGetStatesSortsMap()
+    {
+        $expect = Asst_ConcreteSortableStatus::$sortMap;
+        asort($expect, SORT_NUMERIC);
+        $expect = array_keys($expect);
+
+        $this->assertEquals($expect, Asst_ConcreteSortableStatus::getStates());
+    }
+
+    public function testConstructionSetsCorrectSortValue()
+    {
+        $state1 = new Asst_ConcreteSortableStatus(Asst_ConcreteSortableStatus::STATE_ONE);
+        $state2 = new Asst_ConcreteSortableStatus(Asst_ConcreteSortableStatus::STATE_TWO);
+
+        $this->assertAttributeEquals(1, 'sort', $state1);
+        $this->assertAttributeEquals(2, 'sort', $state2);
+    }
+
+    public function testExceptionIsThrownOnConstructionIfNoSortMapIsDefined()
+    {
+        $this->expectException('\RuntimeException');
+        $this->expectExceptionMessage(sprintf('%s does not define', Asst_InvalidSortableStatus::class));
+
+        $state = new Asst_InvalidSortableStatus(Asst_InvalidSortableStatus::STATE_ONE);
+    }
+
+    public function testExceptionIsThrownOnGetStatesIfNoSortMapIsDefined()
+    {
+        $this->expectException('\RuntimeException');
+        $this->expectExceptionMessage(sprintf('%s does not define', Asst_InvalidSortableStatus::class));
+
+        Asst_InvalidSortableStatus::getStates();
+    }
+}
+
+class Asst_ConcreteSortableStatus extends AbstractSortableStatus
+{
+    const STATE_ONE = 'one';
+    const STATE_TWO = 'two';
+
+    public static $sortMap = [
+        self::STATE_TWO => 2,
+        self::STATE_ONE => 1,
+    ];
+
+
+}
+
+class Asst_InvalidSortableStatus extends AbstractSortableStatus
+{
+    const STATE_ONE = 'one';
+}
---
+++ module/Core/test/CoreTest/Entity/Status/AbstractStatusTest.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @license MIT
+ * @copyright  2013 - 2017 Cross Solution <http://cross-solution.de>
+ */
+  
+/** */
+namespace CoreTest\Entity\Status;
+
+use Core\Entity\Status\AbstractStatus;
+use Core\Entity\Status\StatusInterface;
+use CoreTestUtils\TestCase\TestInheritanceTrait;
+
+/**
+ * Tests for \Core\Entity\Status\AbstractStatus
+ * 
+ * @covers \Core\Entity\Status\AbstractStatus
+ * @author Mathias Gelhausen <gelhausen@cross-solution.de>
+ * @group Core
+ * @group Core.Entity
+ * @group Core.Entity.Status
+ */
+class AbstractStatusTest extends \PHPUnit_Framework_TestCase
+{
+    use TestInheritanceTrait;
+
+    /**
+     *
+     *
+     * @var array|null|Ast_ConcreteStatus
+     */
+    private $target = [
+        Ast_ConcreteStatus::class,
+        [ Ast_ConcreteStatus::TEST ],
+        '@testInheritance' => [
+            'class' => AbstractStatus::class,
+            'as_reflection' => true
+        ],
+        '@testGetStates' => false,
+        '@testConstructionThrowsException' => false,
+        '@testConstruction' => false,
+    ];
+
+    private $inheritance = [ StatusInterface::class ];
+
+    public function testGetStates()
+    {
+        $this->assertEquals(['test', 'state'], Ast_ConcreteStatus::getStates());
+    }
+
+    public function testConstructionThrowsException()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid state name');
+
+        new Ast_ConcreteStatus('invalid');
+    }
+
+    public function testConstruction()
+    {
+        $state = new Ast_ConcreteStatus(Ast_ConcreteStatus::TEST);
+
+        $this->assertAttributeEquals(Ast_ConcreteStatus::TEST, 'state', $state);
+    }
+
+    public function testToString()
+    {
+        $this->assertEquals(Ast_ConcreteStatus::TEST, $this->target->__toString());
+    }
+
+    public function testIs()
+    {
+        $this->assertTrue($this->target->is(Ast_ConcreteStatus::TEST));
+        $this->assertFalse($this->target->is(Ast_ConcreteStatus::STATE));
+        $this->assertTrue($this->target->is(new Ast_ConcreteStatus(Ast_ConcreteStatus::TEST)));
+    }
+}
+
+class Ast_ConcreteStatus extends AbstractStatus
+{
+    const TEST = 'test';
+    const STATE = 'state';
+}
