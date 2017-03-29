e445fa51a0a88394fce1d15fa3d32d5572a47a33
Miroslav Fedeleš <miroslav.fedeles@gmail.com>
2017-03-29 18:30:55 +0200

    #342 Core\Paginator\PaginatorService made not shared by default to allow instantiate different instances in sigle request Core\Repository\AbstractRepository: explicit use of a namespace alias for better IDE namespace resolution (Eclipse was unable to resolve "extends ODM\DocumentRepository" properly)

--- module/Core/src/Core/Repository/AbstractRepository.php
+++ module/Core/src/Core/Repository/AbstractRepository.php
@@ -3,10 +3,11 @@
 namespace Core\Repository;
 
 use Core\Entity\EntityInterface;
-use \Doctrine\ODM\MongoDB as ODM;
+use Doctrine\ODM\MongoDB as ODM;
+use Doctrine\ODM\MongoDB\DocumentRepository;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
-abstract class AbstractRepository extends ODM\DocumentRepository implements RepositoryInterface
+abstract class AbstractRepository extends DocumentRepository implements RepositoryInterface
 {
 
     protected $entityPrototype;
