f92e127bce2665ab3b4356e2dd0d6d35c293fa4e
Mathias Gelhausen <gelhausen@cross-solution.de>
2017-04-19 15:42:51 +0200

    [Core] Code documentation for 'ImagineOptions'

--- module/Core/src/Core/Options/ImagineOptions.php
+++ module/Core/src/Core/Options/ImagineOptions.php
@@ -13,20 +13,30 @@ namespace Core\Options;
 use Zend\Stdlib\AbstractOptions;
 
 /**
- * ${CARET}
+ * Options for the Imagine library.
  * 
  * @author Mathias Gelhausen <gelhausen@cross-solution.de>
- * @todo write test 
+ * @since 0.29
  */
 class ImagineOptions extends AbstractOptions
 {
 
+    /**#@+
+     * Image library.
+     *
+     * @var string
+     */
     const LIB_GD = 'Gd';
-
     const LIB_IMAGICK = 'Imagick';
-
     const LIB_GMAGICK = 'Gmagick';
 
+    /**#@-*/
+
+    /**
+     * The image library to be used.
+     *
+     * @var string
+     */
     protected $imageLib = self::LIB_GD;
 
     /**
