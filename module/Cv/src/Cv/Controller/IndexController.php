7e192b23a508115cc17038b7c76ea42a231dbba2
Mathias Gelhausen <gelhausen@cross-solution.de>
2017-05-10 12:58:31 +0200

    [Cv] Fix #369: Search in Talent-Pool
    * Simplify IndexController: Use PaginationBuilder controller plugin
    * Simplify SearchForm: Use Core\Form\Searchform

--- module/Cv/src/Cv/Controller/IndexController.php
+++ module/Cv/src/Cv/Controller/IndexController.php
@@ -7,7 +7,7 @@
  * @license   MIT
  */
 
-/** ActionController of Core */
+/** */
 namespace Cv\Controller;
 
 use Cv\Form\SearchForm;
@@ -16,80 +16,32 @@ use Zend\View\Model\ViewModel;
 use Zend\Session\Container as Session;
 
 /**
- * Main Action Controller for the application.
- * Responsible for displaying the home site.
  *
+ * @author Mathias Gelhausen <gelhausen@cross-solution.de>
+ * @author Munthi
+ *
+ * @since 0.29 - uses PaginationBuilder controller plugin.
  */
 class IndexController extends AbstractActionController
 {
-    /**
-     * @var SearchForm
-     */
-    protected $searchForm;
-
-    public function __construct(SearchForm $searchForm)
-    {
-        $this->searchForm = $searchForm;
-    }
-
     /**
      * Home site
      *
      */
     public function indexAction()
     {
-        /* @var \Zend\Http\Request $request */
-        $request = $this->getRequest();
-        $params = $request->getQuery();
-        $jsonFormat = 'json' == $request->getQuery()->get('format');
-        $event = $this->getEvent();
-        $routeMatch = $event->getRouteMatch();
-        $matchedRouteName = $routeMatch->getMatchedRouteName();
-        $url = $this->url()->fromRoute($matchedRouteName, array(), array('force_canonical' => true));
+        $result = $this->pagination([
+                'params' => ['Cvs_Index', [
+                    'search',
+                    'page' => 1,
+                    'l',
+                    'd' => 10]
+                ],
+                'form' => ['as' => 'filterForm', 'Cv/SearchForm'],
+                'paginator' => ['as' => 'resumes', 'Cv/Paginator']
+            ]);
 
-
-        if (!$jsonFormat && !$request->isXmlHttpRequest()) {
-            $session = new Session('Cv\Index');
-            $sessionKey = $this->auth()->isLoggedIn() ? 'userParams' : 'guestParams';
-            $sessionParams = $session[$sessionKey];
-            if ($sessionParams) {
-                foreach ($sessionParams as $key => $value) {
-                    $params->set($key, $params->get($key, $value));
-                }
-            }
-            $session[$sessionKey] = $params->toArray();
-
-            $this->searchForm->bind($params);
-        }
-
-        $params = $params->get('params', []);
-
-        if (isset($params['l']['data']) &&
-            isset($params['l']['name']) &&
-            !empty($params['l']['name'])
-        ) {
-            /* @var \Geo\Form\GeoText $geoText */
-            $geoText = $this->searchForm->get('params')->get('l');
-
-            $geoText->setValue($params['l']);
-            $params['location'] = $geoText->getValue('entity');
-        }
-
-
-        $this->searchForm->setAttribute('action', $url);
-        $paginator = $this->paginator('Cv/Paginator', $params);
-
-        $options = $this->searchForm->getOptions();
-        $options['showButtons'] = false;
-        $this->searchForm->setOptions($options);
-
-        $return = array(
-            'resumes' => $paginator,
-            'filterForm' => $this->searchForm
-        );
-        $model = new ViewModel($return);
-
-        return $model;
+        return $result;
     }
 
     public function viewAction()
