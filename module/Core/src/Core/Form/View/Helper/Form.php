cd49dcd7e8279474f2e8546954349324521382ed
Carsten Bleek <bleek@cross-solution.de>
2017-05-30 20:11:18 +0200

    [General] use absolute references for JS assets

--- module/Core/src/Core/Form/View/Helper/Form.php
+++ module/Core/src/Core/Form/View/Helper/Form.php
@@ -70,12 +70,12 @@ class Form extends ZendForm
         $headscript = $renderer->plugin('headscript');
         $basepath   = $renderer->plugin('basepath');
         
-        $headscript->appendFile($basepath('Core/js/core.spinnerbutton.js'))
-                   ->appendFile($basepath('assets/select2/js/select2.min.js'))
-                   ->appendFile($basepath('Core/js/core.forms.js'));
+        $headscript->appendFile($basepath('/Core/js/core.spinnerbutton.js'))
+                   ->appendFile($basepath('/assets/select2/js/select2.min.js'))
+                   ->appendFile($basepath('/Core/js/core.forms.js'));
 
         /* @noinspection PhpParamsInspection */
-        $renderer->headLink()->appendStylesheet($basepath('assets/select2/css/select2.css'));
+        $renderer->headLink()->appendStylesheet($basepath('/assets/select2/css/select2.css'));
 
         if ($scripts = $form->getOption('headscript')) {
             if (!is_array($scripts)) {
