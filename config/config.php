13bb6e6b6e88bdebb83ad1a64e2eea8e034809b6
Anthonius Munthi <me@itstoni.com>
2017-06-08 10:26:57 +0800

    added core zend modules to be used in both install mode or application mode

--- config/config.php
+++ config/config.php
@@ -12,28 +12,24 @@ use Zend\Stdlib\ArrayUtils;
 
 $env = getenv('APPLICATION_ENV') ?: 'production';
 
+$coreModules = [
+	'Zend\ServiceManager\Di',
+	'Zend\Session',
+	'Zend\Router',
+	'Zend\Navigation',
+	'Zend\I18n',
+	'Zend\Mvc\Plugin\Prg',
+	'Zend\Mvc\Plugin\Identity',
+	'Zend\Mvc\Plugin\FlashMessenger',
+	'Zend\Mvc\I18n',
+	'Zend\Form',
+];
 if (!file_exists(__DIR__ . '/autoload/yawik.config.global.php')) {
-    $modules = [
-        'Zend\ServiceManager\Di',
-        'Zend\Session',
-        'Zend\Router',
-        'Zend\Navigation',
-        'Zend\Mvc\Plugin\Prg',
-        'Zend\Mvc\Plugin\Identity',
-        'Zend\Mvc\Plugin\FlashMessenger',
-        'Zend\Mvc\I18n',
+    $modules = array_merge($coreModules,[
         'Install'
-    ];
+    ]);
 } else {
-    $modules = array(
-        'Zend\ServiceManager\Di',
-        'Zend\Session',
-        'Zend\Router',
-        'Zend\Navigation',
-        'Zend\Mvc\Plugin\Prg',
-        'Zend\Mvc\Plugin\Identity',
-        'Zend\Mvc\Plugin\FlashMessenger',
-        'Zend\Mvc\I18n',
+    $modules = array_merge($coreModules,[
         'DoctrineModule',
         'DoctrineMongoODMModule',
         'Core',
@@ -45,7 +41,7 @@ if (!file_exists(__DIR__ . '/autoload/yawik.config.global.php')) {
         'Pdf',
         'Geo',
         'Organizations',
-    );
+    ]);
 
     if (!isset($allModules)) {
         // allModules existiert nur, damit man verschiedene Konfigurationen auf dem gleichen System laufen lassen
