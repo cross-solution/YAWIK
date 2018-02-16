72315f1bb9782abca3741424c9b07b3e53b88a53
Anthonius Munthi <me@itstoni.com>
2018-02-17 00:54:25 +0800

    [Core] improved RepositoryInterface force ::create function to be compatible across implementor. (will cause 7.2 error)

--- module/Core/src/Core/Repository/RepositoryInterface.php
+++ module/Core/src/Core/Repository/RepositoryInterface.php
@@ -9,5 +9,5 @@ interface RepositoryInterface
 {
     public function init(ServiceLocatorInterface $serviceLocator);
     public function setEntityPrototype(EntityInterface $entity);
-    public function create(array $data = null);
+    public function create(array $data = null, $persist = false);
 }
