b7f5205e5a615a70325f05e89e08ee6bd37c5cc3
Carsten Bleek <bleek@cross-solution.de>
2018-09-06 17:15:48 +0200

    injects jobUrl view helper into ApiJobDehydrator

--- module/Jobs/test/JobsTest/Factory/Model/ApiJobDehydratorFactoryTest.php
+++ module/Jobs/test/JobsTest/Factory/Model/ApiJobDehydratorFactoryTest.php
@@ -46,8 +46,12 @@ class ApiJobDehydratorFactoryTest extends \PHPUnit_Framework_TestCase
                         ->disableOriginalConstructor()
                         ->getMock();
 
-        $helpers->expects($this->once())->method('get')
+        $helpers->expects($this->at(0))->method('get')
                 ->with('url')
+                ->willReturn($urlHelper);
+
+        $helpers->expects($this->at(1))->method('get')
+                ->with('jobUrl')
                 ->willReturn($urlHelper);
 
         $serviceManagerMock = $this->getMockBuilder('\Zend\ServiceManager\ServiceManager')
@@ -57,8 +61,7 @@ class ApiJobDehydratorFactoryTest extends \PHPUnit_Framework_TestCase
         $serviceManagerMock->expects($this->once())->method('get')
                            ->with('ViewHelperManager')
                            ->willReturn($helpers);
-
-
+      
         $service = $target->__invoke($serviceManagerMock,'irrelevant');
 
         $this->assertInstanceOf('\Jobs\Model\ApiJobDehydrator', $service);
