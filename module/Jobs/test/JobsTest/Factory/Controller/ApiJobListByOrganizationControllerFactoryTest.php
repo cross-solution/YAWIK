5ef9b6fe450ca3dc5ada3e0e7d5d2fa8b4959b82
Carsten Bleek <bleek@cross-solution.de>
2018-09-06 12:36:18 +0200

    uses "callback" as a query parameter.

---
+++ module/Jobs/test/JobsTest/Factory/Controller/ApiJobListByOrganizationControllerFactoryTest.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * YAWIK
+ *
+ * @filesource
+ * @copyright (c) 2013 - 2016 Cross Solution (http://cross-solution.de)
+ * @license       MIT
+ */
+
+namespace JobsTest\Factory\Controller;
+
+use Jobs\Factory\Controller\ApiJobListByOrganizationControllerFactory;
+use Test\Bootstrap;
+
+/**
+ * Class TApiJobListByOrganizationControllerFactoryTest
+ * @package JobsTest\Factory\Controller
+ */
+class ApiJobListByOrganizationControllerFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ApiJobListByOrganizationControllerFactory
+     */
+    private $testedObj;
+
+    /**
+     *
+     */
+    public function setUp()
+    {
+        $this->testedObj = new ApiJobListByOrganizationControllerFactory();
+    }
+
+    /**
+     *
+     */
+    public function testCreateService()
+    {
+        $sm = clone Bootstrap::getServiceManager();
+        $sm->setAllowOverride(true);
+
+        $jobRepositoryMock = $this->getMockBuilder('Jobs\Repository\Job')
+                                ->disableOriginalConstructor()
+                                ->getMock();
+
+        $repositoriesMock = $this->getMockBuilder('Core\Repository\RepositoryService')
+                               ->disableOriginalConstructor()
+                               ->getMock();
+
+        $repositoriesMock->expects($this->once())
+                       ->method('get')
+                       ->with('Jobs/Job')
+                       ->willReturn($jobRepositoryMock);
+
+        $sm->setService('repositories', $repositoriesMock);
+
+	    $result = $this->testedObj->__invoke($sm,ApiJobListByOrganizationController::class);
+        $this->assertInstanceOf('Jobs\Controller\ApiJobListByOrganizationController', $result);
+    }
+}
