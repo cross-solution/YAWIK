2af6a31bca02fbb122d90ed93ead2cdc9f964d68
Anthonius Munthi <me@itstoni.com>
2018-10-22 15:23:35 +0800

    added missing travis autoload config, ref #466

---
+++ .travis/autoload/asset.config.global.php
@@ -0,0 +1,13 @@
+<?php
+
+$manifest = __DIR__.'/../../public/build/manifest.json';
+if (!is_file($manifest)) {
+    file_put_contents($manifest, '{}', LOCK_EX);
+}
+return [
+    'view_helper_config' => [
+        'asset' => [
+            'resource_map' => json_decode(file_get_contents($manifest), true),
+        ]
+    ]
+];
