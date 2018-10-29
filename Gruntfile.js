35b36740d05b6b268ad650ebb0b4d58f80fbfcd3
Anthonius Munthi <me@itstoni.com>
2018-10-29 23:08:05 +0800

    removed grunt watch command

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,6 @@
-
 global.nodeModulesPath = __dirname + "/node_modules";
-
 module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
-
-    grunt.initConfig({
-        watch: {
-            styles: {
-                files: ['module/**/*.less'], // which files to watch
-                tasks: ['build:dev'],
-                options: {
-                    spawn: false
-                }
-            }
-        }
-    });
-
-    grunt.registerTask('watch',['watch']);
 
     grunt.loadTasks('./public/modules/Core');
     grunt.registerTask('default', ['yawik:core']);
