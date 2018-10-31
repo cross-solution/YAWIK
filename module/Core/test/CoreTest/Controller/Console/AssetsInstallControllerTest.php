e52cefc72d1137b38cba93cb4b0b645c75da4e88
Anthonius Munthi <me@itstoni.com>
2018-10-31 14:02:02 +0800

    fixed file header

--- module/Core/test/CoreTest/Controller/Console/AssetsInstallControllerTest.php
+++ module/Core/test/CoreTest/Controller/Console/AssetsInstallControllerTest.php
@@ -3,8 +3,6 @@
 /*
  * This file is part of the YAWIK project.
  *
- *     (c) Anthonius Munthi <me@itstoni.com>
- *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
