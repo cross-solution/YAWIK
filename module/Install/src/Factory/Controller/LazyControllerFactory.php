4714baa338e43749c2c33589aafed8c6fe333cde
Anthonius Munthi <me@itstoni.com>
2018-11-07 02:01:31 +0800

    [Install] integrated cache service into Install module

--- module/Install/src/Factory/Controller/LazyControllerFactory.php
+++ module/Install/src/Factory/Controller/LazyControllerFactory.php
@@ -8,6 +8,8 @@
  */
 
 namespace Install\Factory\Controller;
+
+use Core\Service\ClearCacheService;
 use Interop\Container\ContainerInterface;
 use Zend\Form\FormElementManager\FormElementManagerV3Polyfill as FormElementManager;
 use Zend\Mvc\I18n\Translator;
@@ -22,55 +24,49 @@ use Zend\Validator\ValidatorPluginManager;
  */
 class LazyControllerFactory implements AbstractFactoryInterface
 {
-	protected $aliases = [
-		FormElementManager::class => 'FormElementManager',
-		ValidatorPluginManager::class => 'ValidatorManager',
-		Translator::class => 'translator',
-	];
-	
-	public function canCreate( ContainerInterface $container, $requestedName )
-	{
-		list( $module, ) = explode( '\\', __NAMESPACE__, 2 );
-		return strstr( $requestedName, $module . '\Controller') !== false;
-	}
-	
-	public function __invoke( ContainerInterface $container, $requestedName, array $options = null )
-	{
-		$class = new \ReflectionClass($requestedName);
-		$parentLocator = $container;
-		if( $constructor = $class->getConstructor() )
-		{
-			if( $params = $constructor->getParameters() )
-			{
-				$parameter_instances = [];
-				foreach( $params as $p )
-				{
-					
-					if( $p->getClass() ) {
-						$cn = $p->getClass()->getName();
-						if (array_key_exists($cn, $this->aliases)) {
-							$cn = $this->aliases[$cn];
-						}
-						
-						try {
-							$parameter_instances[] = $parentLocator->get($cn);
-						}
-						catch (\Exception $x) {
-							echo __CLASS__
-							     . " couldn't create an instance of $cn to satisfy the constructor for $requestedName.";
-							exit;
-						}
-					}
-					else{
-						if( $p->isArray() && $p->getName() == 'config' )
-							$parameter_instances[] = $parentLocator->get('config');
-					}
-					
-				}
-				return $class->newInstanceArgs($parameter_instances);
-			}
-		}
-		
-		return new $requestedName;
-	}
+    protected $aliases = [
+        FormElementManager::class => 'FormElementManager',
+        ValidatorPluginManager::class => 'ValidatorManager',
+        Translator::class => 'translator',
+        ClearCacheService::class => ClearCacheService::class,
+    ];
+    
+    public function canCreate(ContainerInterface $container, $requestedName)
+    {
+        list($module, ) = explode('\\', __NAMESPACE__, 2);
+        return strstr($requestedName, $module . '\Controller') !== false;
+    }
+    
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $class = new \ReflectionClass($requestedName);
+        if ($constructor = $class->getConstructor()) {
+            if ($params = $constructor->getParameters()) {
+                $parameter_instances = [];
+                foreach ($params as $p) {
+                    if ($p->getClass()) {
+                        $cn = $p->getClass()->getName();
+                        if (array_key_exists($cn, $this->aliases)) {
+                            $cn = $this->aliases[$cn];
+                        }
+                        
+                        try {
+                            $parameter_instances[] = $container->get($cn);
+                        } catch (\Exception $x) {
+                            echo __CLASS__
+                                 . " couldn't create an instance of $cn to satisfy the constructor for $requestedName.";
+                            exit;
+                        }
+                    } else {
+                        if ($p->isArray() && $p->getName() == 'config') {
+                            $parameter_instances[] = $container->get('config');
+                        }
+                    }
+                }
+                return $class->newInstanceArgs($parameter_instances);
+            }
+        }
+        
+        return new $requestedName;
+    }
 }
