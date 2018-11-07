bc1e3f8617d19011b72899af93aa79bfe0c3c10f
Anthonius Munthi <me@itstoni.com>
2018-11-08 00:44:48 +0800

    manually executing composer auto-scripts

--- etc/docker/php/docker-entrypoint.sh
+++ etc/docker/php/docker-entrypoint.sh
@@ -7,7 +7,7 @@ if [ "${1#-}" != "$1" ]; then
 fi
 
 # start selenium server
-exec ./bin/start-selenium > /dev/null 2>&1 &
+#./bin/start-selenium > /dev/null 2>&1 &
 umask 0000
 chmod 777 var/cache -Rf
 chmod 777 var/log -Rf
