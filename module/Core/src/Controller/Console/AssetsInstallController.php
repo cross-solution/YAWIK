d18054b4c7eddfef2befff8a8c465457c8a9845d
Anthonius Munthi <me@itstoni.com>
2018-11-13 23:55:27 +0800

    [Core] integrated composer-plugin into main development

--- module/Core/src/Controller/Console/AssetsInstallController.php
+++ module/Core/src/Controller/Console/AssetsInstallController.php
@@ -15,9 +15,8 @@ use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Finder\Finder;
+use Yawik\Composer\AssetsInstaller;
 use Zend\ModuleManager\ModuleManager;
 use Zend\Mvc\Console\Controller\AbstractConsoleController;
 
@@ -30,10 +29,6 @@ use Zend\Mvc\Console\Controller\AbstractConsoleController;
  */
 class AssetsInstallController extends AbstractConsoleController
 {
-    const METHOD_COPY               = 'copy';
-    const METHOD_ABSOLUTE_SYMLINK   = 'absolute symlink';
-    const METHOD_RELATIVE_SYMLINK   = 'relative symlink';
-
     /**
      * @var Filesystem
      */
@@ -53,12 +48,18 @@ class AssetsInstallController extends AbstractConsoleController
      */
     private $output;
 
+    /**
+     * @var AssetsInstaller
+     */
+    private $installer;
+
     public function __construct(array $modules = [])
     {
-        $this->modules = $modules;
-        $this->filesystem = new Filesystem();
-        $this->input = new ArgvInput();
-        $this->output = new ConsoleOutput();
+        $this->modules      = $modules;
+        $this->filesystem   = new Filesystem();
+        $this->input        = new ArgvInput();
+        $this->output       = new ConsoleOutput();
+        $this->installer    = new AssetsInstaller();
     }
 
     public static function factory(ContainerInterface $container)
@@ -98,73 +99,36 @@ class AssetsInstallController extends AbstractConsoleController
         return $this->output;
     }
 
+    public function setInstaller(AssetsInstaller $installer)
+    {
+        $this->installer = $installer;
+    }
+
     public function indexAction()
     {
         /* @var \Zend\Console\Request $request */
+        $modules        = $this->modules;
+        $request        = $this->getRequest();
+        $symlink        = $request->getParam('symlink');
+        $relative       = $request->getParam('relative');
+        $installer      = $this->installer;
 
-
-        $modules = $this->modules;
+        // load module assets
         foreach ($modules as $module) {
             $this->processModule($module);
         }
 
-        $io = new SymfonyStyle($this->input, $this->output);
-        $io->newLine();
-
-        $rows = [];
-        $exitCode = 0;
-        $copyUsed = false;
-
-        $request = $this->getRequest();
-        $publicDir = $request->getParam('target', getcwd().'/public/modules');
-        $symlink = $request->getParam('symlink');
-        $relative = $request->getParam('relative');
-
-        ksort($this->assets);
-        foreach ($this->assets as $name => $originDir) {
-            $targetDir = $publicDir.DIRECTORY_SEPARATOR.$name;
-            $message = $name;
-            try {
-                $this->filesystem->remove($targetDir);
-
-                if ($relative) {
-                    $expectedMethod = self::METHOD_RELATIVE_SYMLINK;
-                    $method = $this->relativeSymlinkWithFallback($originDir, $targetDir);
-                } elseif ($symlink) {
-                    $expectedMethod = self::METHOD_ABSOLUTE_SYMLINK;
-                    $method = $this->absoluteSymlinkWithFallback($originDir, $targetDir);
-                } else {
-                    $expectedMethod = self::METHOD_COPY;
-                    $method = $this->hardCopy($originDir, $targetDir);
-                }
-
-                if (self::METHOD_COPY === $method) {
-                    $copyUsed = true;
-                }
-
-                if ($method === $expectedMethod) {
-                    $rows[] = array(sprintf('<fg=green;options=bold>%s</>', '\\' === DIRECTORY_SEPARATOR ? 'OK' : "\xE2\x9C\x94" /* HEAVY CHECK MARK (U+2714) */), $message, $method);
-                } else {
-                    $rows[] = array(sprintf('<fg=yellow;options=bold>%s</>', '\\' === DIRECTORY_SEPARATOR ? 'WARNING' : '!'), $message, $method);
-                }
-            } catch (\Exception $e) {
-                $exitCode = 1;
-                $rows[] = array(sprintf('<fg=red;options=bold>%s</>', '\\' === DIRECTORY_SEPARATOR ? 'ERROR' : "\xE2\x9C\x98" /* HEAVY BALLOT X (U+2718) */), $message, $e->getMessage());
-            }
-        }
-
-        if ($rows) {
-            $io->table(array('', 'Module', 'Method / Error'), $rows);
-        }
-
-        if (0 !== $exitCode) {
-            $io->error('Some errors occurred while installing assets.');
+        // setup expected method
+        if ($relative) {
+            $expectedMethod = AssetsInstaller::METHOD_RELATIVE_SYMLINK;
+        } elseif ($symlink) {
+            $expectedMethod = AssetsInstaller::METHOD_ABSOLUTE_SYMLINK;
         } else {
-            if ($copyUsed) {
-                $io->note('Some assets were installed via copy. If you make changes to these assets you have to run this command again.');
-            }
-            $io->success($rows ? 'All assets were successfully installed.' : 'No assets were provided by any bundle.');
+            $expectedMethod = AssetsInstaller::METHOD_COPY;
         }
+        $installer->setInput($this->input);
+        $installer->setOutput($this->output);
+        $installer->install($this->assets, $expectedMethod);
     }
 
     private function processModule($module)
@@ -183,73 +147,5 @@ class AssetsInstallController extends AbstractConsoleController
         $className = get_class($module);
         $moduleName = substr($className, 0, strpos($className, '\\'));
         $this->assets[$moduleName] = $dir;
-    }
-
-    /**
-     * Try to create absolute symlink.
-     *
-     * Falling back to hard copy.
-     */
-    private function absoluteSymlinkWithFallback($originDir, $targetDir)
-    {
-        try {
-            $this->symlink($originDir, $targetDir);
-            $method = self::METHOD_ABSOLUTE_SYMLINK;
-        } catch (\Exception $e) {
-            // fall back to copy
-            $method = $this->hardCopy($originDir, $targetDir);
-        }
-
-        return $method;
-    }
-
-    /**
-     * Try to create relative symlink.
-     *
-     * Falling back to absolute symlink and finally hard copy.
-     */
-    private function relativeSymlinkWithFallback($originDir, $targetDir)
-    {
-        try {
-            $this->symlink($originDir, $targetDir, true);
-            $method = self::METHOD_RELATIVE_SYMLINK;
-        } catch (\Exception $e) {
-            $method = $this->absoluteSymlinkWithFallback($originDir, $targetDir);
-        }
-
-        return $method;
-    }
-
-    /**
-     * Creates symbolic link.
-     *
-     * @throws \Exception if link can not be created
-     */
-    private function symlink($originDir, $targetDir, $relative = false)
-    {
-        if ($relative) {
-            $this->filesystem->mkdir(dirname($targetDir));
-            $originDir = $this->filesystem->makePathRelative($originDir, realpath(dirname($targetDir)));
-        }
-        $this->filesystem->symlink($originDir, $targetDir);
-        if (!file_exists($targetDir)) {
-            throw new \Exception(
-                sprintf('Symbolic link "%s" was created but appears to be broken.', $targetDir),
-                0,
-                null
-            );
-        }
-    }
-
-    /**
-     * Copies origin to target.
-     */
-    private function hardCopy($originDir, $targetDir)
-    {
-        $this->filesystem->mkdir($targetDir, 0777);
-        // We use a custom iterator to ignore VCS files
-        $this->filesystem->mirror($originDir, $targetDir, Finder::create()->ignoreDotFiles(false)->in($originDir));
-
-        return self::METHOD_COPY;
     }
 }
