fae9957071e93385df22cdfa7995ff923745c6a8
Anthonius Munthi <me@itstoni.com>
2018-11-21 15:32:07 +0800

    added default configuration template

---
+++ module/ReleaseTools/config/release.local.config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'release' => [
+        /*
+         * Change this value to match with your remote
+         * configuration that pointing to: git@github.com:cross-solution/YAWIK.git
+         */
+        'main_remote_name' => 'origin',
+
+        /*
+         * If you don't want to keep git clone for yawik/* repository
+         * in your local directories just leave this value commented.
+         */
+        //'subsplit_clone_dir' => sys_get_temp_dir().'/yawik/build',
+    ],
+];
