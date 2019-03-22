de8b615da71e06b157d34b9ba69592b186b116f9
Carsten Bleek <bleek@cross-solution.de>
2019-03-22 17:39:09 +0100

    sets version to 0.3.1

--- module/Core/src/Module.php
+++ module/Core/src/Module.php
@@ -43,7 +43,7 @@ class Module implements
     RequireFilePermissionInterface,
     RequireDirectoryPermissionInterface
 {
-    const VERSION = '0.34-dev';
+    const VERSION = '0.33.1';
 
     /**
      * @param ModuleOptions $options
