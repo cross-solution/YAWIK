c091c4253ca1ea8f18491991f7a7013f6246a91d
Mathias Gelhausen <gelhausen@cross-solution.de>
2019-03-29 11:30:59 +0100

    Release 0.33.8

--- module/Core/src/Module.php
+++ module/Core/src/Module.php
@@ -43,7 +43,7 @@ class Module implements
     RequireFilePermissionInterface,
     RequireDirectoryPermissionInterface
 {
-    const VERSION = '0.33.7';
+    const VERSION = '0.33.8';
 
     /**
      * @param ModuleOptions $options
