2ee7212eead61a49a06f1b6a9ddcb1da95a0c8ae
Carsten Bleek <bleek@cross-solution.de>
2020-07-20 12:38:19 +0200

    fix: typo in german translation
    https://github.com/cross-solution/YAWIK/issues/587

--- module/Organizations/language/_annotated_strings.php
+++ module/Organizations/language/_annotated_strings.php
@@ -1,142 +1,148 @@
 <?php
-//: ./config/module.config.php:195
-translate('Overview');
-
-//: ./config/module.config.php:199
-translate('Profiles');
-
-//: ./config/module.config.php:203
-translate('Insert');
-
 //: ./view/form/invite-employee-bar.phtml:33
 translate('Invite an employee via email address.');
 
-//: ./src/Organizations/Mail/EmployeeInvitationFactory.php:93
-translate('Invitation to join the team of %s');
+//: ./src/Controller/IndexController.php:110
+translate('Search for organizations');
 
-//: ./src/Organizations/Form/EmployeeFieldset.php:61
-translate('View Jobs');
-
-//: ./src/Organizations/Form/EmployeeFieldset.php:62
-translate('Edit Jobs');
-
-//: ./src/Organizations/Form/EmployeeFieldset.php:63
-translate('Create Jobs');
-
-//: ./src/Organizations/Form/EmployeeFieldset.php:64
-translate('View Applications');
-
-//: ./src/Organizations/Form/EmployeeFieldset.php:65
-translate('Edit Applications');
-
-//: ./src/Organizations/Form/EmployeeFieldset.php:77
-translate('Recruiter');
-
-//: ./src/Organizations/Form/EmployeeFieldset.php:78
-translate('Department Manager');
-
-//: ./src/Organizations/Form/EmployeeFieldset.php:79
-translate('Managing Directors');
-
-//: ./src/Organizations/Form/OrganizationsNameFieldset.php:77
-translate('Organization Name');
-
-//: ./src/Organizations/Form/OrganizationsProfileFieldset.php:47
-translate('Setting');
-
-//: ./src/Organizations/Form/OrganizationsProfileFieldset.php:49
-translate('Always enable profile');
-
-//: ./src/Organizations/Form/OrganizationsProfileFieldset.php:50
-translate('Enable only when active jobs available');
-
-//: ./src/Organizations/Form/OrganizationsProfileFieldset.php:51
-translate('Disabled viewing profile');
-
-//: ./src/Organizations/Form/OrganizationsDescriptionFieldset.php:44
-translate('Description');
-
-//: ./src/Organizations/Form/EmployeesFieldset.php:56
-translate('Invite an employee via email address.');
-
-//: ./src/Organizations/Form/WorkflowSettingsFieldset.php:52
-translate('accept Applications by Department Managers');
-
-//: ./src/Organizations/Form/WorkflowSettingsFieldset.php:53
-translate('if checked, department managers are informed about new applications first.');
-
-//: ./src/Organizations/Form/WorkflowSettingsFieldset.php:54
-translate('Department managers are notified of incoming applications and must accept this. Only then the recruiter can start his work with the application');
-
-//: ./src/Organizations/Form/WorkflowSettingsFieldset.php:65
-translate('assign department managers to jobs');
-
-//: ./src/Organizations/Form/WorkflowSettingsFieldset.php:66
-translate('if checked, department managers have to be assigned to job postings.');
-
-//: ./src/Organizations/Form/WorkflowSettingsFieldset.php:67
-translate('if you have more them one department managers, you can assign them to a job posting. If nobody is assigned, all department managers will be informed about new applications');
-
-//: ./src/Organizations/Form/Organizations.php:36
-translate('Please enter the name of the hiring organization.');
-
-//: ./src/Organizations/Form/Organizations.php:45
-translate('Please enter a contact for the hiring organization.');
-
-//: ./src/Organizations/Form/Organizations.php:64
-translate('Please enter a description for the hiring organization.');
-
-//: ./src/Organizations/Form/Organizations.php:72
-translate('Employees');
-
-//: ./src/Organizations/Form/Organizations.php:74
-translate('Manage your employees and their permissions.');
-
-//: ./src/Organizations/Form/Organizations.php:82
-translate('Workflow');
-
-//: ./src/Organizations/Form/Organizations.php:84
-translate('Define, how notifications about new applications reach your employees');
-
-//: ./src/Organizations/Form/Organizations.php:92
-translate('Profile Setting');
-
-//: ./src/Organizations/Form/Organizations.php:94
-translate('Define how profile page should behave');
-
-//: ./src/Organizations/Form/LogoImageFactory.php:52
-translate('Choose a Logo. This logo will be shown in the job opening and the application form.');
-
-//: ./src/Organizations/Form/OrganizationsContactFieldset.php:50
-translate('Street');
-
-//: ./src/Organizations/Form/OrganizationsContactFieldset.php:59
-translate('House Number');
-
-//: ./src/Organizations/Form/OrganizationsContactFieldset.php:68
-translate('Postal Code');
-
-//: ./src/Organizations/Form/OrganizationsContactFieldset.php:77
-translate('City');
-
-//: ./src/Organizations/Form/OrganizationsContactFieldset.php:85
-translate('Country');
-
-//: ./src/Organizations/Form/OrganizationsContactFieldset.php:93
-translate('Phone');
-
-//: ./src/Organizations/Form/OrganizationsContactFieldset.php:101
-translate('Fax');
-
-//: ./src/Organizations/Controller/ProfileController.php:136
-translate('This Organization Profile is disabled');
-
-//: ./src/Organizations/Controller/InviteEmployeeController.php:92
+//: ./src/Controller/InviteEmployeeController.php:91
 translate('The organization referenced in your request could not be found.');
 
-//: ./src/Organizations/Controller/InviteEmployeeController.php:98
+//: ./src/Controller/InviteEmployeeController.php:97
 translate('The access token you provided seems to have expired.');
 
-//: ./src/Organizations/Controller/IndexController.php:111
-translate('Search for organizations');
+//: ./src/Mail/EmployeeInvitationFactory.php:93
+translate('Invitation to join the team of %s');
+
+//: ./src/Form/EmployeeFieldset.php:60
+translate('View Jobs');
+
+//: ./src/Form/EmployeeFieldset.php:61
+translate('Edit Jobs');
+
+//: ./src/Form/EmployeeFieldset.php:62
+translate('Create Jobs');
+
+//: ./src/Form/EmployeeFieldset.php:63
+translate('View Applications');
+
+//: ./src/Form/EmployeeFieldset.php:64
+translate('Edit Applications');
+
+//: ./src/Form/EmployeeFieldset.php:76
+translate('Recruiter');
+
+//: ./src/Form/EmployeeFieldset.php:77
+translate('Department Manager');
+
+//: ./src/Form/EmployeeFieldset.php:78
+translate('Managing Directors');
+
+//: ./src/Form/OrganizationsContactFieldset.php:51
+translate('Street');
+
+//: ./src/Form/OrganizationsContactFieldset.php:60
+translate('House Number');
+
+//: ./src/Form/OrganizationsContactFieldset.php:69
+translate('Postal Code');
+
+//: ./src/Form/OrganizationsContactFieldset.php:78
+translate('City');
+
+//: ./src/Form/OrganizationsContactFieldset.php:86
+translate('Country');
+
+//: ./src/Form/OrganizationsContactFieldset.php:94
+translate('Phone');
+
+//: ./src/Form/OrganizationsContactFieldset.php:102
+translate('Fax');
+
+//: ./src/Form/LogoImageFactory.php:52
+translate('Choose a Logo. This logo will be shown in the job opening and the application form.');
+
+//: ./src/Form/Organizations.php:35
+translate('Please enter the name of the hiring organization.');
+
+//: ./src/Form/Organizations.php:44
+translate('Please enter a contact for the hiring organization.');
+
+//: ./src/Form/Organizations.php:63
+translate('Please enter a description for the hiring organization.');
+
+//: ./src/Form/Organizations.php:71
+translate('Employees');
+
+//: ./src/Form/Organizations.php:73
+translate('Manage your employees and their permissions.');
+
+//: ./src/Form/Organizations.php:81
+translate('Workflow');
+
+//: ./src/Form/Organizations.php:83
+translate('Define, how notifications about new applications reach your employees');
+
+//: ./src/Form/Organizations.php:91
+translate('Profile Setting');
+
+//: ./src/Form/Organizations.php:93
+translate('Define how profile page should behave');
+
+//: ./src/Form/OrganizationsProfileFieldset.php:46
+translate('Setting');
+
+//: ./src/Form/OrganizationsProfileFieldset.php:48
+translate('Always enable profile');
+
+//: ./src/Form/OrganizationsProfileFieldset.php:49
+translate('Enable only when active jobs available');
+
+//: ./src/Form/OrganizationsProfileFieldset.php:50
+translate('Disabled viewing profile');
+
+//: ./src/Form/EmployeesFieldset.php:55
+translate('Invite an employee via email address.');
+
+//: ./src/Form/OrganizationsNameFieldset.php:74
+translate('Organization Name');
+
+//: ./src/Form/OrganizationsDescriptionFieldset.php:44
+translate('Description');
+
+//: ./src/Form/WorkflowSettingsFieldset.php:50
+translate('accept Applications by Department Managers');
+
+//: ./src/Form/WorkflowSettingsFieldset.php:51
+translate('if checked, department managers are informed about new applications first.');
+
+//: ./src/Form/WorkflowSettingsFieldset.php:52
+translate('Department managers are notified of incoming applications and must accept this. Only then the recruiter can start his work with the application');
+
+//: ./src/Form/WorkflowSettingsFieldset.php:63
+translate('accept Applications by recruiters');
+
+//: ./src/Form/WorkflowSettingsFieldset.php:64
+translate('if checked, all recruiters of the organization are informed about new applications first.');
+
+//: ./src/Form/WorkflowSettingsFieldset.php:65
+translate('Recruiters are notified of incoming applications and must accept/reject them. Accepted applications are forwarded to the department manager assigned to the job.');
+
+//: ./src/Form/WorkflowSettingsFieldset.php:76
+translate('assign department managers to jobs');
+
+//: ./src/Form/WorkflowSettingsFieldset.php:77
+translate('if checked, department managers have to be assigned to job postings.');
+
+//: ./src/Form/WorkflowSettingsFieldset.php:78
+translate('if you have more them one department managers, you can assign them to a job posting. If nobody is assigned, all department managers will be informed about new applications');
+
+//: ./config/module.config.php:196
+translate('Overview');
+
+//: ./config/module.config.php:200
+translate('Profiles');
+
+//: ./config/module.config.php:204
+translate('Insert');
 
