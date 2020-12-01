6fa25e4e4f3dccdd6228bf1938d1666cb2ceb0db
Carsten Bleek <bleek@cross-solution.de>
2020-12-01 10:38:54 +0100

    add libpng-dev

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -7,7 +7,7 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git zlib1g-dev libicu-dev g++ -yqq
+apt-get install git libpng-dev zlib1g-dev libicu-dev g++ -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
