94e3de619bb5d62f9b0bf0f43e679c222be7aa65
Carsten Bleek <bleek@cross-solution.de>
2020-12-01 14:23:15 +0100

    ci/docker_install.sh

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -7,7 +7,13 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git php php-mongodb php-gd php-intl unzip libpng-dev zlib1g-dev libicu-dev g++ -yqq
+apt-get install git \
+        php-pear \
+	php7.4 \
+	php7.4-mongodb \
+	php7.4-gd \
+	php7.4-intl \
+	unzip libpng-dev zlib1g-dev libicu-dev g++ -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl -sS --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
