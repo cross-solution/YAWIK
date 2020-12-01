0739365873e3464dd810c7d2558e056fc5642b65
Carsten Bleek <bleek@cross-solution.de>
2020-12-01 14:46:07 +0100

    add install nodejs

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -8,9 +8,11 @@ set -xe
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
 apt-get install git \
-	nodejs \
 	unzip libpng-dev zlib1g-dev libicu-dev g++ -yqq
 
+# install nodejs
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+apt-get install -y nodejs
 
 # Install phpunit, the tool that we will use for testing
 curl -sS --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
