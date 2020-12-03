b90c4abe79058e02f38a339cbc3d1803ce09e40f
Carsten Bleek <bleek@cross-solution.de>
2020-12-03 12:48:51 +0100

    try to access db

--- etc/travis/autoload/yawik.config.global.php
+++ etc/travis/autoload/yawik.config.global.php
@@ -7,7 +7,7 @@ return array(
                 array(
                     'odm_default' =>
                         array(
-                            'connectionString' => 'mongodb://mongodb:27017/YAWIK',
+                            'connectionString' => 'mongodb://db:27017/YAWIK',
                         ),
                 ),
             'configuration' =>
