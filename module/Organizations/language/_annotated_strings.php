08a43e24ad4d7eb9bb2429db70e7b4d52eb7724c
Carsten Bleek <bleek@cross-solution.de>
2021-01-20 22:45:12 +0100

    chore: add hungarian for install, organizations and settings

--- module/Organizations/language/_annotated_strings.php
+++ module/Organizations/language/_annotated_strings.php
@@ -1,9 +1,15 @@
 <?php
+//: ./config/module.config.php:196
+translate('Overview');
+
+//: ./config/module.config.php:200
+translate('Profiles');
+
+//: ./config/module.config.php:204
+translate('Insert');
+
 //: ./view/form/invite-employee-bar.phtml:33
 translate('Invite an employee via email address.');
-
-//: ./src/Controller/IndexController.php:110
-translate('Search for organizations');
 
 //: ./src/Controller/InviteEmployeeController.php:91
 translate('The organization referenced in your request could not be found.');
@@ -11,32 +17,11 @@ translate('The organization referenced in your request could not be found.');
 //: ./src/Controller/InviteEmployeeController.php:97
 translate('The access token you provided seems to have expired.');
 
+//: ./src/Controller/IndexController.php:110
+translate('Search for organizations');
+
 //: ./src/Mail/EmployeeInvitationFactory.php:93
 translate('Invitation to join the team of %s');
-
-//: ./src/Form/EmployeeFieldset.php:60
-translate('View Jobs');
-
-//: ./src/Form/EmployeeFieldset.php:61
-translate('Edit Jobs');
-
-//: ./src/Form/EmployeeFieldset.php:62
-translate('Create Jobs');
-
-//: ./src/Form/EmployeeFieldset.php:63
-translate('View Applications');
-
-//: ./src/Form/EmployeeFieldset.php:64
-translate('Edit Applications');
-
-//: ./src/Form/EmployeeFieldset.php:76
-translate('Recruiter');
-
-//: ./src/Form/EmployeeFieldset.php:77
-translate('Department Manager');
-
-//: ./src/Form/EmployeeFieldset.php:78
-translate('Managing Directors');
 
 //: ./src/Form/OrganizationsContactFieldset.php:51
 translate('Street');
@@ -59,8 +44,8 @@ translate('Phone');
 //: ./src/Form/OrganizationsContactFieldset.php:102
 translate('Fax');
 
-//: ./src/Form/LogoImageFactory.php:52
-translate('Choose a Logo. This logo will be shown in the job opening and the application form.');
+//: ./src/Form/EmployeesFieldset.php:55
+translate('Invite an employee via email address.');
 
 //: ./src/Form/Organizations.php:35
 translate('Please enter the name of the hiring organization.');
@@ -89,26 +74,8 @@ translate('Profile Setting');
 //: ./src/Form/Organizations.php:93
 translate('Define how profile page should behave');
 
-//: ./src/Form/OrganizationsProfileFieldset.php:46
-translate('Setting');
-
-//: ./src/Form/OrganizationsProfileFieldset.php:48
-translate('Always enable profile');
-
-//: ./src/Form/OrganizationsProfileFieldset.php:49
-translate('Enable only when active jobs available');
-
-//: ./src/Form/OrganizationsProfileFieldset.php:50
-translate('Disabled viewing profile');
-
-//: ./src/Form/EmployeesFieldset.php:55
-translate('Invite an employee via email address.');
-
-//: ./src/Form/OrganizationsNameFieldset.php:74
-translate('Organization Name');
-
-//: ./src/Form/OrganizationsDescriptionFieldset.php:44
-translate('Description');
+//: ./src/Form/LogoImageFactory.php:52
+translate('Choose a Logo. This logo will be shown in the job opening and the application form.');
 
 //: ./src/Form/WorkflowSettingsFieldset.php:50
 translate('accept Applications by Department Managers');
@@ -137,12 +104,45 @@ translate('if checked, department managers have to be assigned to job postings.'
 //: ./src/Form/WorkflowSettingsFieldset.php:78
 translate('if you have more them one department managers, you can assign them to a job posting. If nobody is assigned, all department managers will be informed about new applications');
 
-//: ./config/module.config.php:196
-translate('Overview');
+//: ./src/Form/OrganizationsDescriptionFieldset.php:44
+translate('Description');
 
-//: ./config/module.config.php:200
-translate('Profiles');
+//: ./src/Form/OrganizationsProfileFieldset.php:46
+translate('Setting');
 
-//: ./config/module.config.php:204
-translate('Insert');
+//: ./src/Form/OrganizationsProfileFieldset.php:48
+translate('Always enable profile');
+
+//: ./src/Form/OrganizationsProfileFieldset.php:49
+translate('Enable only when active jobs available');
+
+//: ./src/Form/OrganizationsProfileFieldset.php:50
+translate('Disabled viewing profile');
+
+//: ./src/Form/OrganizationsNameFieldset.php:74
+translate('Organization Name');
+
+//: ./src/Form/EmployeeFieldset.php:60
+translate('View Jobs');
+
+//: ./src/Form/EmployeeFieldset.php:61
+translate('Edit Jobs');
+
+//: ./src/Form/EmployeeFieldset.php:62
+translate('Create Jobs');
+
+//: ./src/Form/EmployeeFieldset.php:63
+translate('View Applications');
+
+//: ./src/Form/EmployeeFieldset.php:64
+translate('Edit Applications');
+
+//: ./src/Form/EmployeeFieldset.php:76
+translate('Recruiter');
+
+//: ./src/Form/EmployeeFieldset.php:77
+translate('Department Manager');
+
+//: ./src/Form/EmployeeFieldset.php:78
+translate('Managing Directors');
 
